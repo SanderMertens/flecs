5d4cbb47490951b02b06fa4ab0296d7e2beee62a
Sander Mertens <sander.mertens8@gmail.com>
2018-10-26 08:23:59 -0700

    Add test for 2 NOT from component columns

--- test/api/src/EcsPeriodicSystem.c
+++ test/api/src/EcsPeriodicSystem.c
@@ -1294,3 +1294,57 @@ void test_EcsPeriodicSystem_tc_system_from_component_not(
 
     ecs_fini(world);
 }
+
+void test_EcsPeriodicSystem_tc_system_2_from_component_not(
+    test_EcsPeriodicSystem this)
+{
+    Context ctx = {0};
+    EcsWorld *world = ecs_init();
+    ECS_COMPONENT(world, Foo);
+    ECS_COMPONENT(world, Bar);
+    ECS_FAMILY(world, Family1, EcsComponent, Foo);
+    ECS_FAMILY(world, Family2, EcsComponent, Bar);
+    ECS_SYSTEM(world, TestSystem, EcsPeriodic, Foo, !COMPONENT.Foo, !COMPONENT.Bar);
+
+    EcsHandle e1 = ecs_new(world, Family1_h);
+    EcsHandle e2 = ecs_new(world, Family2_h);
+    EcsHandle e3 = ecs_new(world, Foo_h);
+    EcsHandle e4 = ecs_new(world, Foo_h);
+    EcsHandle e5 = ecs_new(world, Foo_h);
+
+    test_assert(e1 != 0);
+    test_assert(e2 != 0);
+    test_assert(e3 != 0);
+    test_assert(e4 != 0);
+    test_assert(e5 != 0);
+
+    ecs_add(world, e3, e1);
+    ecs_commit(world, e3);
+    ecs_add(world, e4, e2);
+    ecs_commit(world, e4);
+
+    ecs_add(world, e4, e1);
+    ecs_add(world, e4, e2);
+    ecs_commit(world, e4);
+
+    *(int*)ecs_get(world, e1, Foo_h) = 10;
+    *(int*)ecs_get(world, e2, Bar_h) = 20;
+    *(int*)ecs_get(world, e3, Foo_h) = 30;
+    *(int*)ecs_get(world, e4, Foo_h) = 40;
+    *(int*)ecs_get(world, e5, Foo_h) = 50;
+
+    ecs_set_context(world, &ctx);
+
+    ecs_progress(world);
+
+    test_assertint(ctx.column_count, 1);
+    test_assertint(ctx.count, 2);
+    test_assert(ctx.entities[0] == e1);
+    test_assert(ctx.entities[1] == e5);
+    test_assertint(ctx.column[0][0], 10);
+    test_assertint(ctx.column[0][1], 50);
+    test_assertint(ctx.component[0][0], Foo_h);
+    test_assertint(ctx.component[0][1], Foo_h);
+
+    ecs_fini(world);
+}
