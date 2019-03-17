217fd3e75514dd0ee92e67ef5724744e34203238
Sander Mertens <sander.mertens8@gmail.com>
2019-03-16 22:48:50 -0700

    Add movement to graphics template

--- templates/flecs-graphics/src/main.c
+++ templates/flecs-graphics/src/main.c
@@ -1,30 +1,50 @@
 #include <include/${id base}.h>
 
+#define MOVE_SPEED (5)
+
+void MoveSquare(EcsRows *rows) {
+    EcsInput *input = ecs_column(rows, EcsInput, 1);
+    EcsPosition2D *position = ecs_shared(rows, EcsPosition2D, 2);
+
+    int x_v = input->keys[ECS_KEY_D].state || input->keys[ECS_KEY_RIGHT].state;
+        x_v -= input->keys[ECS_KEY_A].state || input->keys[ECS_KEY_LEFT].state;
+    int y_v = input->keys[ECS_KEY_S].state || input->keys[ECS_KEY_DOWN].state;
+        y_v -= input->keys[ECS_KEY_W].state || input->keys[ECS_KEY_UP].state;
+
+    position->x += x_v * MOVE_SPEED;
+    position->y += y_v * MOVE_SPEED;
+}
+
 int main(int argc, char *argv[]) {
     EcsWorld *world = ecs_init();
 
     ECS_IMPORT(world, EcsComponentsTransform, ECS_2D);
     ECS_IMPORT(world, EcsComponentsGeometry, ECS_2D);
     ECS_IMPORT(world, EcsComponentsGraphics, ECS_2D);
+    ECS_IMPORT(world, EcsComponentsInput, ECS_2D);
     ECS_IMPORT(world, EcsSystemsSdl2, ECS_2D);
 
-    EcsEntity canvas = ecs_set(world, 0, EcsCanvas2D, {
-        .window = {
-            .width = 800,
-            .height = 600,
-        },
-        .title = "Hello ${id}"
+    /* Define entity for square and canvas (SDL window) */
+    ECS_ENTITY(world, Square, EcsPosition2D, EcsSquare, EcsColor);
+
+    /* Define system that moves the square on keyboard input */
+    ECS_SYSTEM(world, MoveSquare, EcsOnFrame, EcsInput, Square.EcsPosition2D);
+
+    /* Initialize canvas */
+    ecs_set(world, 0, EcsCanvas2D, {
+        .window = { .width = 800, .height = 600, }, .title = "Hello ${id}!" 
     });
 
-    EcsEntity square = ecs_set(world, 0, EcsPosition2D, {0, 0});
+    /* Initialize square */
+    ecs_set(world, Square, EcsPosition2D, {0, 0});
+    ecs_set(world, Square, EcsSquare, { .size = 50 });
+    ecs_set(world, Square, EcsColor, { .r = 0, .g = 0, .b = 255, .a = 255 });
 
-    ecs_set(world, square, EcsSquare, {
-        .size = 50
-    });
-
+    /* Enter main loop */
     ecs_set_target_fps(world, 60);
 
     while ( ecs_progress(world, 0));
 
+    /* Cleanup */
     return ecs_fini(world);
 }
