789821947f7b0e902e693e1965da1194fc2c649d
ArnCarver <arncis96@gmail.com>
2019-03-28 00:00:51 +0200

    removed `FLECS_SHARED` macro

--- include/bake_config.h
+++ include/bake_config.h
@@ -33,7 +33,7 @@
 /* Convenience macro for exporting symbols */
 #if FLECS_STATIC
 #define FLECS_EXPORT
-#elif FLECS_SHARED
+#else
 #if FLECS_IMPL && defined _MSC_VER
 #define FLECS_EXPORT __declspec(dllexport)
 #elif FLECS_IMPL
@@ -43,8 +43,6 @@
 #else
 #define FLECS_EXPORT
 #endif
-#else
-#define FLECS_EXPORT
 #endif
 #endif
 
