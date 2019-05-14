5ec4fdda25cf095e5e9ab4d3a5b3c6187347e5fe
Sander Mertens <sander.mertens8@gmail.com>
2019-05-14 07:36:36 -0700

    Add template for module projects

---
+++ templates/flecs-module/include/__id_base.h
@@ -0,0 +1,35 @@
+#ifndef ${id upper}_H
+#define ${id upper}_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+typedef int MyComponent;
+*/
+
+typedef struct ${id pascalcase} {
+    /*
+     * ECS_DECLARE_ENTITY(MyTag);
+     * ECS_DECLARE_COMPONENT(MyComponent);
+     */
+} ${id pascalcase};
+
+void ${id pascalcase}Import(
+    ecs_world_t *world,
+    int flags);
+
+#define ${id pascalcase}ImportHandles(handles) /*\
+    * ECS_IMPORT_ENTITY(handles, MyTag);\
+    * ECS_IMPORT_COMPONENT(handles, MyComponent); 
+    */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ templates/flecs-module/src/main.c
@@ -0,0 +1,16 @@
+#include <include/${id base}.h>
+
+void ${id pascalcase}Import(
+    ecs_world_t *world,
+    int flags)
+{
+    ECS_MODULE(world, ${id pascalcase});
+
+    /*
+     * ECS_COMPONENT(world, MyComponent);
+     * ECS_TAG(world, MyTag);
+     * 
+     * ECS_EXPORT_COMPONENT(MyComponent);
+     * ECS_EXPORT_ENTITY(MyTag);
+     */
+}
