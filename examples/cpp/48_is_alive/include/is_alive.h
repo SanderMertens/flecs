9bf0277d0d8ccf083794056be6bcda35d2fecf84
Sander Mertens <sander.mertens8@gmail.com>
2020-10-04 16:55:22 -0700

    #124 add examples for deferring & is_alive

---
+++ examples/cpp/48_is_alive/include/is_alive.h
@@ -0,0 +1,16 @@
+#ifndef IS_ALIVE_H
+#define IS_ALIVE_H
+
+/* This generated file contains includes for project dependencies */
+#include "is_alive/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ examples/cpp/48_is_alive/src/main.cpp
@@ -0,0 +1,21 @@
+#include <is_alive.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    flecs::world ecs;
+
+    // Create new entity, will be alive
+    auto e_1 = ecs.entity();
+    std::cout << "e_1 alive: " << e_1.is_alive() << std::endl;
+    
+    // Entity will not be alive after deleting
+    e_1.destruct();
+    std::cout << "e_1 alive: " << e_1.is_alive() << std::endl;
+
+    // Create new entity, will return same id but new generation
+    auto e_2 = ecs.entity();
+
+    // e_2 is alive, but e_1 is not because the generation increased
+    std::cout << "e_1 alive: " << e_1.is_alive() << std::endl;
+    std::cout << "e_2 alive: " << e_2.is_alive() << std::endl;
+}
