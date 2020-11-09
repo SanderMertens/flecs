11052868b6ae6a2aaf8e8b919351c2bccad3afd1
Mark Gerl <68554825+markgerl-NG@users.noreply.github.com>
2020-11-09 09:17:00 -0500

    removed extraneous (double) cast per PR review

--- src/modules/pipeline/worker.c
+++ src/modules/pipeline/worker.c
@@ -246,7 +246,7 @@ void ecs_workers_progress(
     }
 
     if (world->measure_frame_time) {
-        world->stats.system_time_total += (double)ecs_time_measure(&start);
+        world->stats.system_time_total += ecs_time_measure(&start);
     }    
 }
 
