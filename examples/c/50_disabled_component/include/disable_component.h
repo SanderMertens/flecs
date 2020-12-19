824b21d82b02c4a42a5090e06233edcffe9d8edc
Sander Mertens <sander.mertens8@gmail.com>
2020-12-18 20:48:21 -0800

    #280 add examples

---
+++ examples/c/50_disabled_component/include/disable_component.h
@@ -0,0 +1,16 @@
+#ifndef DISABLE_COMPONENT_H
+#define DISABLE_COMPONENT_H
+
+/* This generated file contains includes for project dependencies */
+#include "disable_component/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
