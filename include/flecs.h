8e9135e1ad6eccc95d962af07cabd9f2380b6e1a
Randy <randy408@protonmail.com>
2021-03-17 20:09:15 +0100

    ecs_get_world(): update param description

--- include/flecs.h
+++ include/flecs.h
@@ -2989,7 +2989,7 @@ ecs_world_t* ecs_get_stage(
     int32_t stage_id);
 
 /** Get actual world from world
- * @param world A thread-specific pointer to the world.
+ * @param world A pointer to a stage or the world.
  * @return The world.
  */
 FLECS_API
