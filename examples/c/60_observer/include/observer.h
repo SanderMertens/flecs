75109267729cf9163465ad25aed617b9b2217878
Sander Mertens <sander.mertens8@gmail.com>
2021-05-29 05:31:43 -0700

    #422 add observer examples

---
+++ examples/c/60_observer/include/observer.h
@@ -0,0 +1,16 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+/* This generated file contains includes for project dependencies */
+#include "observer/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
