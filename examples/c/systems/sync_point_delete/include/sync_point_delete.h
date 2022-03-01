fbb863e71252a7b250b54fb58b3892708903f640
Sander Mertens <sander.mertens8@gmail.com>
2022-03-01 14:13:03 -0800

    #124 add sync point examples

---
+++ examples/c/systems/sync_point_delete/include/sync_point_delete.h
@@ -0,0 +1,16 @@
+#ifndef SYNC_POINT_DELETE_H
+#define SYNC_POINT_DELETE_H
+
+/* This generated file contains includes for project dependencies */
+#include "sync_point_delete/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
