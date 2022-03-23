fc08ea4c3e5927cfa76070f3ae9996fad1f0e1c6
Sander Mertens <sander.mertens8@gmail.com>
2022-03-23 00:06:25 -0700

    #124 add explorer examples

---
+++ examples/cpp/explorer/include/explorer.h
@@ -0,0 +1,16 @@
+#ifndef EXPLORER_H
+#define EXPLORER_H
+
+/* This generated file contains includes for project dependencies */
+#include "explorer/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ examples/cpp/explorer/src/main.cpp
@@ -0,0 +1,36 @@
+#include <explorer.h>
+#include <iostream>
+
+using mass = flecs::units::mass;
+
+struct Mass {
+    double value;
+};
+
+int main(int argc, char *argv[]) {
+    // Passing in the command line arguments will allow the explorer to display
+    // the application name.
+    flecs::world world(argc, argv);
+
+    world.import<flecs::units>();
+
+    // Mass component
+    world.component<Mass>()
+        .member<double, mass::KiloGrams>("value");
+
+    // Simple hierarchy
+    auto Sun = world.entity("Sun")
+        .set<Mass>({1.988500e31});
+
+    auto Earth = world.scope(Sun).entity("Earth")
+        .set<Mass>({5.9722e24});
+
+    world.scope(Earth).entity("Moon")
+        .set<Mass>({7.34767309e22});
+    
+    // Run application with REST interface. When the application is running,
+    // navigate to https://flecs.dev/explorer to inspect it!
+    //
+    // See docs/RestApi.md#explorer for more information.
+    return world.app().enable_rest().run();
+}
