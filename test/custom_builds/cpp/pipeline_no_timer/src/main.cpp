1ed96ac787a9f5dfb83b5eb36f1edf39e33b22d7
Sander Mertens <sander.mertens8@gmail.com>
2022-05-02 09:06:01 +0200

    #710 add test for custom build without TIMER addon

---
+++ test/custom_builds/cpp/pipeline_no_timer/src/main.cpp
@@ -0,0 +1,30 @@
+#include <pipeline_no_timer.h>
+#include <iostream>
+
+struct Position { 
+    double x, y;
+};
+
+struct Velocity { 
+    double x, y;
+};
+
+int main(int, char *[]) {
+    flecs::world ecs;
+
+    ecs.system<Position, const Velocity>()
+        .each([](Position& p, const Velocity& v) {
+            p.x += v.x;
+            p.y += v.y;
+        });
+
+    auto e = ecs.entity()
+        .set<Position>({10, 20})
+        .set<Velocity>({1, 2});
+
+    ecs.progress();
+
+    const Position *p = e.get<Position>();
+    assert(p->x == 11);
+    assert(p->y == 22);
+}
