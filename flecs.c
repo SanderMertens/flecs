bf8eb6854759c293bdfdeb02491d1250d8025d11
Sander Mertens <sander.mertens8@gmail.com>
2022-05-05 16:21:39 +0200

    #124 add basic cycle check to ecs_search_relation

--- flecs.c
+++ flecs.c
@@ -38669,6 +38669,8 @@ int32_t type_search_relation(
 
             ecs_table_t *obj_table = rec->table;
             if (obj_table) {
+                ecs_assert(obj_table != table, ECS_CYCLE_DETECTED, NULL);
+                
                 r = type_search_relation(world, obj_table, 0, id, idr, 
                     rel, idr_r, min_depth - 1, max_depth - 1, subject_out, 
                     id_out, depth_out, tr_out);
