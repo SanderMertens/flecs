d67d929f6cadde894b86a5dc2b73bd0b1d175f5a
Sander Mertens <sander.mertens8@gmail.com>
2022-06-14 19:14:48 +0000

    Fix potential Wundef warnings

--- include/flecs/bake_config.h
+++ include/flecs/bake_config.h
@@ -22,11 +22,11 @@
 
 /* Convenience macro for exporting symbols */
 #ifndef flecs_STATIC
-#if flecs_EXPORTS && (defined(_MSC_VER) || defined(__MINGW32__))
+#if defined(flecs_EXPORTS) && (defined(_MSC_VER) || defined(__MINGW32__))
   #define FLECS_API __declspec(dllexport)
-#elif flecs_EXPORTS
+#elif defined(flecs_EXPORTS)
   #define FLECS_API __attribute__((__visibility__("default")))
-#elif defined _MSC_VER
+#elif defined(_MSC_VER)
   #define FLECS_API __declspec(dllimport)
 #else
   #define FLECS_API
