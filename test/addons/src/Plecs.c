582b2b2e2c856828d53217ec8d51724c2969eab4
Sander Mertens <sander.mertens8@gmail.com>
2022-09-22 19:30:18 -0700

    Fix plecs issue that would interpret '-' on newline as binary expression

--- test/addons/src/Plecs.c
+++ test/addons/src/Plecs.c
@@ -4058,3 +4058,34 @@ void Plecs_const_var_scoped() {
 
     ecs_fini(world);
 }
+
+void Plecs_scope_w_component_after_const_var() {
+    ecs_world_t *world = ecs_init();
+
+    ecs_entity_t ecs_id(Position) = ecs_struct(world, {
+        .entity = ecs_entity(world, {.name = "Position"}),
+        .members = {
+            {"x", ecs_id(ecs_f32_t)},
+            {"y", ecs_id(ecs_f32_t)}
+        }
+    });
+
+    const char *expr =
+    HEAD "Foo {"
+    LINE "  const var = 5"
+    LINE "  - Position{x: 10, y: $var}"
+    LINE "}";
+
+    test_assert(ecs_plecs_from_str(world, NULL, expr) == 0);
+
+    ecs_entity_t foo = ecs_lookup_fullpath(world, "Foo");
+    test_assert(foo != 0);
+    test_assert(ecs_has(world, foo, Position));
+
+    const Position *p = ecs_get(world, foo, Position);
+    test_assert(p != NULL);
+    test_int(p->x, 10);
+    test_int(p->y, 5);
+
+    ecs_fini(world);
+}
