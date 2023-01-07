db65463941c6b101a43c7c109adc833b63cdcb95
Sander Mertens <sander.mertens8@gmail.com>
2023-01-07 14:57:32 -0500

    Add example for entity::emplace

---
+++ examples/cpp/entities/emplace/include/emplace.h
@@ -0,0 +1,16 @@
+#ifndef EMPLACE_H
+#define EMPLACE_H
+
+/* This generated file contains includes for project dependencies */
+#include "emplace/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
