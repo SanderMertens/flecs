b48d0d75a0167c101f42333dbf79297f9273174d
Sander Mertens <sander.mertens8@gmail.com>
2023-06-19 18:10:41 -0700

    Add member range examples, meta header documentation

---
+++ examples/c/reflection/member_ranges/include/member_ranges.h
@@ -0,0 +1,16 @@
+#ifndef MEMBER_RANGES_H
+#define MEMBER_RANGES_H
+
+/* This generated file contains includes for project dependencies */
+#include "member_ranges/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
