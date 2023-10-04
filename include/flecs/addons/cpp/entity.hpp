fe2d9698498d314dab09af043c89c69e746f6030
Indradb <60851042+Indra-db@users.noreply.github.com>
2023-10-04 04:13:03 +0100

    #1055 Remove redundant component registration from entity::ref methods
    for the standard get_ref I'm passing it as a parameter, which should ensure registration too
    
    for the other get_refs, it's already being registered in the function parameter passthrough too.

--- include/flecs/addons/cpp/entity.hpp
+++ include/flecs/addons/cpp/entity.hpp
@@ -223,16 +223,12 @@ struct entity : entity_builder<entity>
      */
     template <typename T>
     ref<T> get_ref() const {
-        // Ensure component is registered
-        _::cpp_type<T>::id(m_world);
-        return ref<T>(m_world, m_id);
+        return ref<T>(m_world, m_id, _::cpp_type<T>::id(m_world));
     }
 
     template <typename First, typename Second, typename P = flecs::pair<First, Second>, 
         typename A = actual_type_t<P>>
     ref<A> get_ref() const {
-        // Ensure component is registered
-        _::cpp_type<A>::id(m_world);
         return ref<A>(m_world, m_id, 
             ecs_pair(_::cpp_type<First>::id(m_world),
                 _::cpp_type<Second>::id(m_world)));
@@ -240,16 +236,12 @@ struct entity : entity_builder<entity>
 
     template <typename First>
     ref<First> get_ref(flecs::entity_t second) const {
-        // Ensure component is registered
-        _::cpp_type<First>::id(m_world);
         return ref<First>(m_world, m_id, 
             ecs_pair(_::cpp_type<First>::id(m_world), second));
     }
 
     template <typename Second>
     ref<Second> get_ref_second(flecs::entity_t first) const {
-        // Ensure component is registered
-        _::cpp_type<Second>::id(m_world);
         return ref<Second>(m_world, m_id, 
             ecs_pair(first, _::cpp_type<Second>::id(m_world)));
     }
