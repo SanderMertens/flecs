9b73ef959bc72640c0b97d881e933a0b49b9e7a4
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2023-12-16 22:07:12 -0500

    Revert "Unreal Flecs general"
    This reverts commit 544f5f1764872d72672effc95554a23fe02cd1f5.

--- Source/UnrealFlecs/UnrealFlecs.Build.cs
+++ Source/UnrealFlecs/UnrealFlecs.Build.cs
@@ -26,7 +26,7 @@ public class UnrealFlecs : ModuleRules
 			new string[]
 			{
 				"Core",
-				"FlecsLibrary"
+				// ... add other public dependencies that you statically link with here ...
 			}
 			);
 			
@@ -38,7 +38,7 @@ public class UnrealFlecs : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				"SolidMacros"
+				// ... add private dependencies that you statically link with here ...	
 			}
 			);
 		
--- Source/UnrealFlecs/UnrealFlecs.cpp
+++ Source/UnrealFlecs/UnrealFlecs.cpp
@@ -6,11 +6,13 @@
 
 void FUnrealFlecsModule::StartupModule()
 {
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
 void FUnrealFlecsModule::ShutdownModule()
 {
-	
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
