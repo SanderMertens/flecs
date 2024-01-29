199aebdb01585983b6065cedcd4e1094b26007ac
Sander Mertens <sander.mertens8@gmail.com>
2024-01-29 03:07:59 -0800

    Add parameter to REST API for new rows JSON format

--- src/addons/rest.c
+++ src/addons/rest.c
@@ -191,6 +191,7 @@ void flecs_rest_parse_json_ser_iter_params(
     flecs_rest_bool_param(req, "duration", &desc->measure_eval_duration);
     flecs_rest_bool_param(req, "type_info", &desc->serialize_type_info);
     flecs_rest_bool_param(req, "table", &desc->serialize_table);
+    flecs_rest_bool_param(req, "rows", &desc->serialize_rows);
 }
 
 static
