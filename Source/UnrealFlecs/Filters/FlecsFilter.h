6477fbd79a07616c9d67e9cc8ccfdce997546989
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-02-03 15:40:57 -0500

    Update FlecsFilter.h

--- Source/UnrealFlecs/Filters/FlecsFilter.h
+++ Source/UnrealFlecs/Filters/FlecsFilter.h
@@ -12,6 +12,28 @@ struct FFlecsFilter
     GENERATED_BODY()
 
 public:
+    FORCEINLINE FFlecsFilter() = default;
+    FORCEINLINE FFlecsFilter(const flecs::filter_base& InFilter) : Filter(InFilter) {}
+
+    FORCEINLINE void SetFilter(const flecs::filter_base& InFilter) { Filter = InFilter; }
+
+    FORCEINLINE flecs::filter_base& GetFlecsFilter() { return Filter; }
+    FORCEINLINE const flecs::filter_base& GetFlecsFilter() const { return Filter; }
+
+    FORCEINLINE operator flecs::filter_base&() { return GetFlecsFilter(); }
+    FORCEINLINE operator const flecs::filter_base&() const { return GetFlecsFilter(); }
+    FORCEINLINE operator flecs::filter_base*() { return &Filter; }
+
+    FORCEINLINE bool operator==(const FFlecsFilter& Other) const
+    {
+        return Filter == Other.Filter;
+    }
+
+    FORCEINLINE bool operator!=(const FFlecsFilter& Other) const
+    {
+        return Filter != Other.Filter;
+    }
 
 private:
+    flecs::filter_base Filter;
 }; // struct FFlecsFilter
