bc3b70aca56f33a52f09816fd6ffd2112d6c33ed
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-02-03 15:47:16 -0500

    Update FlecsQuery.h

--- Source/UnrealFlecs/Queries/FlecsQuery.h
+++ Source/UnrealFlecs/Queries/FlecsQuery.h
@@ -3,10 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "flecs.h"
 #include "FlecsQuery.generated.h"
 
 USTRUCT(BlueprintType)
 struct FFlecsQuery
 {
     GENERATED_BODY()
+
+public:
+    FORCEINLINE FFlecsQuery() = default;
+    FORCEINLINE FFlecsQuery(const flecs::query_base& InQuery) : Query(InQuery) {}
+    FORCEINLINE FFlecsQuery(const flecs::query_base* InQuery) : Query(*InQuery) {}
+
+    FORCEINLINE void SetQuery(const flecs::query_base& InQuery) { Query = InQuery; }
+    FORCEINLINE void SetQuery(const flecs::query_base* InQuery) { Query = *InQuery; }
+
+    FORCEINLINE flecs::query_base& GetFlecsQuery() { return Query; }
+    FORCEINLINE const flecs::query_base& GetFlecsQuery() const { return Query; }
+
+    FORCEINLINE operator flecs::query_base&() { return GetFlecsQuery(); }
+    FORCEINLINE operator const flecs::query_base&() const { return GetFlecsQuery(); }
+
+private:
+    flecs::query_base Query;
 }; // struct FFlecsQuery
