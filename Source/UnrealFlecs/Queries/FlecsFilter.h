fe1a949e46b4da4b11b028b18a1fa6e1348e3822
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-02-05 13:50:43 -0500

    Added ForEach and Iterate Functions to FilterType

--- Source/UnrealFlecs/Queries/FlecsFilter.h
+++ Source/UnrealFlecs/Queries/FlecsFilter.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "flecs.h"
+#include "Entities/FlecsEntityHandle.h"
 #include "SolidMacros/Macros.h"
+#include "Worlds/FlecsWorld.h"
 #include "FlecsFilter.generated.h"
 
 USTRUCT(BlueprintType)
@@ -35,6 +37,52 @@ public:
     FORCEINLINE NO_DISCARD bool operator!=(const FFlecsFilter& Other) const
     {
         return Filter != Other.Filter;
+    }
+    
+    FORCEINLINE void Set(const flecs::filter<>& InFilter)
+    {
+        Filter = InFilter;
+    }
+
+    FORCEINLINE NO_DISCARD int32 GetCount() const
+    {
+        return Filter.count();
+    }
+
+    template <typename FunctionType>
+    FORCEINLINE void ForEach(FunctionType Function) const
+    {
+        Filter.each(Function);
+    }
+
+    template <typename FunctionType>
+    FORCEINLINE void ForEach(FFlecsWorld& InWorld, FunctionType Function) const
+    {
+        Filter.each(InWorld, Function);
+    }
+
+    template <typename FunctionType>
+    FORCEINLINE void ForEach(const FFlecsEntityHandle& InEntity, FunctionType Function) const
+    {
+        Filter.each(InEntity, Function);
+    }
+
+    template <typename FunctionType>
+    FORCEINLINE void Iterate(FunctionType Function) const
+    {
+        Filter.iter(Function);
+    }
+
+    template <typename FunctionType>
+    FORCEINLINE void Iterate(FFlecsWorld& InWorld, FunctionType Function) const
+    {
+        Filter.iter(InWorld, Function);
+    }
+
+    template <typename FunctionType>
+    FORCEINLINE void Iterate(const FFlecsEntityHandle& InEntity, FunctionType Function) const
+    {
+        Filter.iter(InEntity, Function);
     }
 
 private:
