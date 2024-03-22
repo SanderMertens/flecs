4a178cad5cfd31324387d5e2fe4937443aedc12c
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-03-22 15:24:45 -0400

    Removed Unneeded Files

--- test/custom_builds/c/custom_header/src/main.c
+++
@@ -1,9 +0,0 @@
-#include <custom_header.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
-    assert(HELLOWORLD);
-    return 0;
-}
--- test/custom_builds/cpp/ensure_define/src/main.cpp
+++
@@ -1,9 +0,0 @@
-// Unreal (apparently) defines an ensure macro which conflicts with ensure
-// methods in the C++ API. This test makes sure this doesn't break compilation.
-#define ensure foobar
-#include <ensure_define.h>
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-    flecs::world world(argc, argv);
-}
