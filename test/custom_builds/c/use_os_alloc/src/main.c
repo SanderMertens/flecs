874981087bd5201ba6d2838882a37f32b8e67764
Sander Mertens <sander.mertens8@gmail.com>
2024-04-15 00:14:27 -0700

    Fix custom build test

--- test/custom_builds/c/use_os_alloc/src/main.c
+++ test/custom_builds/c/use_os_alloc/src/main.c
@@ -4,13 +4,7 @@
 int main(int argc, char *argv[]) {
     ecs_world_t *world = ecs_init_w_args(argc, argv);
 
-    /* Set target FPS for main loop */
-    ecs_set_target_fps(world, 60);
-
-    printf("Application use_os_alloc is running, press CTRL-C to exit...\n");
-
-    /* Run systems */
-    while ( ecs_progress(world, 0));
+    ecs_progress(world, 0.0);
 
     return ecs_fini(world);
 }
