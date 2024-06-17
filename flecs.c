97a2fcd8fb69487ebb54a13ad576abfd53f414a4
Sander Mertens <sander.mertens8@gmail.com>
2024-06-17 02:45:04 -0700

    Fix issue with escaping doc strings

--- flecs.c
+++ flecs.c
@@ -42364,7 +42364,7 @@ void flecs_json_serialize_iter_this(
         flecs_json_object_push(buf);
         if (this_data->label) {
             flecs_json_memberl(buf, "label");
-            flecs_json_string(buf, this_data->label[row].value);
+            flecs_json_string_escape(buf, this_data->label[row].value);
         } else {
             flecs_json_memberl(buf, "label");
             if (this_data->names) {
@@ -42379,22 +42379,22 @@ void flecs_json_serialize_iter_this(
 
         if (this_data->brief) {
             flecs_json_memberl(buf, "brief");
-            flecs_json_string(buf, this_data->brief[row].value);
+            flecs_json_string_escape(buf, this_data->brief[row].value);
         }
 
         if (this_data->detail) {
             flecs_json_memberl(buf, "detail");
-            flecs_json_string(buf, this_data->detail[row].value);
+            flecs_json_string_escape(buf, this_data->detail[row].value);
         }
 
         if (this_data->color) {
             flecs_json_memberl(buf, "color");
-            flecs_json_string(buf, this_data->color[row].value);
+            flecs_json_string_escape(buf, this_data->color[row].value);
         }
 
         if (this_data->link) {
             flecs_json_memberl(buf, "link");
-            flecs_json_string(buf, this_data->link[row].value);
+            flecs_json_string_escape(buf, this_data->link[row].value);
         }
 
         flecs_json_object_pop(buf);
@@ -44429,7 +44429,9 @@ int ecs_world_to_json_buf(
 
     ecs_iter_t it = ecs_query_iter(world, q);
     ecs_iter_to_json_desc_t json_desc = { 
-        .serialize_table = true
+        .serialize_table = true,
+        .serialize_full_paths = true,
+        .serialize_entity_ids = true
     };
 
     int ret = ecs_iter_to_json_buf(&it, buf_out, &json_desc);
