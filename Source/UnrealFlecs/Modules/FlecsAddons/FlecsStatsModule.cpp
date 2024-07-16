72416f5b438a294cc4e05231c1ee90d730d3f464
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-07-16 07:18:48 -0400

    Added Flecs Stats Module impl

---
+++ Source/UnrealFlecs/Modules/FlecsAddons/FlecsStatsModule.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FlecsStatsModule.h"
+#include "Worlds/FlecsWorld.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FlecsStatsModule)
+
+void UFlecsStatsModule::InitializeModule(UFlecsWorld* InWorld, const FFlecsEntityHandle& InModuleEntity)
+{
+	InModuleEntity.Destroy();
+	ModuleEntity = InWorld->World.import<flecs::stats>();
+}
+
+void UFlecsStatsModule::DeinitializeModule(UFlecsWorld* InWorld)
+{
+	ModuleEntity.Destroy();
+}
---
+++ Source/UnrealFlecs/Modules/FlecsAddons/FlecsStatsModule.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/FlecsModuleInterface.h"
+#include "UObject/Object.h"
+#include "FlecsStatsModule.generated.h"
+
+UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
+class UNREALFLECS_API UFlecsStatsModule final : public UObject, public IFlecsModuleInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void InitializeModule(UFlecsWorld* InWorld, const FFlecsEntityHandle& InModuleEntity) override;
+	virtual void DeinitializeModule(UFlecsWorld* InWorld) override;
+
+}; // class UFlecsStatsModule
