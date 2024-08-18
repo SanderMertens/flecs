285434abc6325d5844dedf6b5e70e6d81af33fb7
dom <domdom@users.noreply.github.com>
2024-08-18 14:57:26 +1000

    Add missing import to c++ stats module
    This fixes #1300

--- flecs.h
+++ flecs.h
@@ -31760,6 +31760,10 @@ inline units::units(flecs::world& world) {
 namespace flecs {
 
 inline stats::stats(flecs::world& world) {
+#ifdef FLECS_UNITS
+    world.import<flecs::units>();
+#endif
+
     /* Import C module  */
     FlecsStatsImport(world);
 
