386acc521ddc904bc647b7bd18c3151ac4d53bd8
Paulo Rafael Feodrippe <pfeodrippe@gmail.com>
2024-10-02 15:55:57 +0000

    #1383 Add FLECS_NO_ALERTS support

--- distr/flecs.h
+++ distr/flecs.h
@@ -10314,6 +10314,9 @@ int ecs_value_move_ctor(
 #ifdef FLECS_NO_SYSTEM
 #undef FLECS_SYSTEM
 #endif
+#ifdef FLECS_NO_ALERTS
+#undef FLECS_ALERTS
+#endif
 #ifdef FLECS_NO_PIPELINE
 #undef FLECS_PIPELINE
 #endif
