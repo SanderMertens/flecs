544cccc7659d5ef0dd98141fee6bde82465284b4
Sander Mertens <sander.mertens8@gmail.com>
2024-11-15 23:56:57 +0800

    #1440 Add multiple_queries C++ example

---
+++ examples/cpp/systems/multiple_queries/include/multiple_queries.h
@@ -0,0 +1,16 @@
+#ifndef MULTIPLE_QUERIES_H
+#define MULTIPLE_QUERIES_H
+
+/* This generated file contains includes for project dependencies */
+#include "multiple_queries/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
