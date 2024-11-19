eb4ea4d9c2720ca9bc318ca8fbd695b5042e431a
Sander Mertens <sander.mertens8@gmail.com>
2024-11-18 21:58:45 -0800

    #1445 Add .to_json() method to flecs::query_base

--- distr/flecs.h
+++ distr/flecs.h
@@ -30222,6 +30222,20 @@ struct query_base {
 
     operator query<>() const;
 
+#   ifdef FLECS_JSON
+
+/** Serialize query to JSON.
+ * 
+ * @memberof flecs::query_base
+ * @ingroup cpp_addons_json
+ */
+flecs::string to_json(flecs::iter_to_json_desc_t *desc = nullptr) {
+    ecs_iter_t it = ecs_query_iter(ecs_get_world(query_), query_);
+    char *json = ecs_iter_to_json(&it, desc);
+    return flecs::string(json);
+}
+#   endif
+
 protected:
     query_t *query_ = nullptr;
 };
