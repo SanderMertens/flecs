69aaf027b1e484980cdfda050cb1f0905048dc52
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-11-21 15:47:29 -0500

    Separated WorldName and World Entity, and Fixed FString Singleton

---
+++ Source/UnrealFlecs/Components/FlecsWorldNameComponent.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FlecsWorldNameComponent.generated.h"
+
+USTRUCT(BlueprintType)
+struct UNREALFLECS_API FFlecsWorldNameComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Flecs | World")
+	FString WorldName;
+	
+}; // struct FFlecsWorldNameComponent
+
