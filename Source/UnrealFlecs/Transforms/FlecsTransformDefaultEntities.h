5f5c70c6ce000bcfa051482bf11a356f8544aabf
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-12-06 11:43:07 -0500

    Update FlecsTransformDefaultEntities.h

--- Source/UnrealFlecs/Transforms/FlecsTransformDefaultEntities.h
+++ Source/UnrealFlecs/Transforms/FlecsTransformDefaultEntities.h
@@ -5,23 +5,24 @@
 #include "CoreMinimal.h"
 #include "flecs.h"
 #include "Entities/FlecsDefaultEntityEngineSubsystem.h"
-#include "FlecsTransformDefaultEntities.generated.h"
+//#include "FlecsTransformDefaultEntities.generated.h"
 
+// Gets the closest Entity with FGlobalTransform
 DEFINE_DEFAULT_ENTITY_OPTION(FlecsLocalTrait);
 
-// May be deprecated
+// Maybe deprecated
 DEFINE_DEFAULT_ENTITY_OPTION(FlecsGlobalTrait);
 
-USTRUCT(BlueprintType)
+/*USTRUCT(BlueprintType)
 struct UNREALFLECS_API FFlecsRelativeTrait
 {
 	GENERATED_BODY()
 	
 	/* How far up the hierarchy to look for the relative entity, 0 is the Parent(Which is FlecsLocalTrait),
-	 * 1 is the Parent's Parent, etc.*/
+	 * 1 is the Parent's Parent, etc.#1#
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Flecs | Transform")
 	int32 RelativeToIndex = 0;
 	
 }; // struct FFlecsRelativeComponent
 
-DEFINE_DEFAULT_ENTITY_OPTION(FFlecsRelativeRelationTrait);
+DEFINE_DEFAULT_ENTITY_OPTION(FFlecsRelativeRelationTrait);*/
