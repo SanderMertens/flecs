6979e3945a1cb7c22fd326d77bcb6d13234b062c
Sander Mertens <sander.mertens8@gmail.com>
2024-12-08 03:48:29 +0000

    Add more script examples

---
+++ examples/c/script/functions/include/functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+/* This generated file contains includes for project dependencies */
+#include "functions/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ examples/c/script/script_w_vars/include/script_w_vars.h
@@ -0,0 +1,16 @@
+#ifndef SCRIPT_W_VARS_H
+#define SCRIPT_W_VARS_H
+
+/* This generated file contains includes for project dependencies */
+#include "script_w_vars/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
