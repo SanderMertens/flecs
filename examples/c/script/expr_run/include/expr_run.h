d942964214ac26d96eab43fd2ac21cb2963fed1f
Sander Mertens <sander.mertens8@gmail.com>
2024-12-08 02:21:00 +0000

    Add flecs script examples

---
+++ examples/c/script/expr_run/include/expr_run.h
@@ -0,0 +1,16 @@
+#ifndef EXPR_RUN_H
+#define EXPR_RUN_H
+
+/* This generated file contains includes for project dependencies */
+#include "expr_run/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
