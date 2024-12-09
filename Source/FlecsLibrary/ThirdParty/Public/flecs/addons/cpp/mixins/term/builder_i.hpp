5e392f477d8cb33c50326f6efbe0ce8ce9f1d127
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-12-09 12:03:36 -0500

    Fixed optional Builder doc

--- Source/FlecsLibrary/ThirdParty/Public/flecs/addons/cpp/mixins/term/builder_i.hpp
+++ Source/FlecsLibrary/ThirdParty/Public/flecs/addons/cpp/mixins/term/builder_i.hpp
@@ -365,7 +365,7 @@ struct term_builder_i : term_ref_builder_i<Base> {
         return this->oper(flecs::Not);
     }
 
-    /* Short for oper(flecs::Or) */
+    /* Short for oper(flecs::Optional) */
     Base& optional() {
         return this->oper(flecs::Optional);
     }
