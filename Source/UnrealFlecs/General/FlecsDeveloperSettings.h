b214e88c28f4ce61e2d7ed5c1a3d09a54d86b3ee
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-12-23 12:54:06 -0500

    Moved FlecsDeveloperSettings to Game Config

--- Source/UnrealFlecs/General/FlecsDeveloperSettings.h
+++ Source/UnrealFlecs/General/FlecsDeveloperSettings.h
@@ -6,8 +6,8 @@
 #include "Engine/DeveloperSettings.h"
 #include "FlecsDeveloperSettings.generated.h"
 
-UCLASS(BlueprintType, Config = Flecs, DefaultConfig, Category = "Flecs",
-	meta = (DisplayName = "Flecs Developer Settings"))
+UCLASS(BlueprintType, Config = Game, DefaultConfig, Category = "Flecs",
+	meta = (DisplayName = "Flecs World Settings"))
 class UNREALFLECS_API UFlecsDeveloperSettings final : public UDeveloperSettings
 {
 	GENERATED_BODY()
