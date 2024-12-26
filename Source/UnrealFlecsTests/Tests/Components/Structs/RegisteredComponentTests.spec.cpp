2848bff0c1a927f2e8e2d9ef01173451a9af827b
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-12-26 18:08:13 -0500

    Set Ptr Test

--- Source/UnrealFlecsTests/Tests/Components/Structs/RegisteredComponentTests.spec.cpp
+++ Source/UnrealFlecsTests/Tests/Components/Structs/RegisteredComponentTests.spec.cpp
@@ -31,8 +31,7 @@ void FRegisteredComponentTestsSpec::Define()
 				TestEntity.Has(FUStructTestComponent::StaticStruct()));
 		});
 
-		It("Should add USTRUCT Component using cpp type",
-			[this]()
+		It("Should add USTRUCT Component using cpp type", [this]()
 		{
 			const FFlecsEntityHandle TestEntity = Fixture.FlecsWorld->CreateEntity();
 			TestEntity.Add<FUStructTestComponent>();
@@ -43,14 +42,30 @@ void FRegisteredComponentTestsSpec::Define()
 				TestEntity.Has(FUStructTestComponent::StaticStruct()));
 		});
 
-		It("Should set USTRUCT Component using FInstancedStruct",
-			[this]()
+		It("Should set USTRUCT Component using FInstancedStruct", [this]()
 		{
 			FInstancedStruct TestComponent
 				= FInstancedStruct(FUStructTestComponent::StaticStruct());
 				TestComponent.InitializeAs<FUStructTestComponent>(FUStructTestComponent{ 1 });
 			const FFlecsEntityHandle TestEntity = Fixture.FlecsWorld->CreateEntity();
 			TestEntity.Set(TestComponent);
+
+			TestTrue("Component properties should be registered",
+				TestEntity.Has<FUStructTestComponent>());
+				
+			if (TestTrue("Component properties should be registered",
+				TestEntity.Has(FUStructTestComponent::StaticStruct())))
+			{
+				TestEqual("Component properties should be registered",
+					TestEntity.Get<FUStructTestComponent>().Value, 1);
+			}
+		});
+
+		It("Should set USTRUCT Component using Set Ptr", [this]()
+		{
+			constexpr FUStructTestComponent TestComponent{ 1 };
+			const FFlecsEntityHandle TestEntity = Fixture.FlecsWorld->CreateEntity();
+			TestEntity.Set(FUStructTestComponent::StaticStruct(), &TestComponent);
 
 			TestTrue("Component properties should be registered",
 				TestEntity.Has<FUStructTestComponent>());
