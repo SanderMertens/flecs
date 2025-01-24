2eac23f7b4f3de5eee84c1ad4fcaece1410be1c8
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-01-24 11:05:49 -0500

    Update flecs.h

--- Source/FlecsLibrary/Public/flecs.h
+++ Source/FlecsLibrary/Public/flecs.h
@@ -211,18 +211,6 @@
  #define FLECS_UNITS          /**< Builtin standard units */
 #endif // ifndef FLECS_CUSTOM_BUILD
 
-/** @def FLECS_LOW_FOOTPRINT
- * Set a number of constants to values that decrease memory footprint, at the
- * cost of decreased performance. */
-// #define FLECS_LOW_FOOTPRINT
-// #ifdef FLECS_LOW_FOOTPRINT
-// #define FLECS_HI_COMPONENT_ID (16)
-// #define FLECS_HI_ID_RECORD_ID (16)
-// #define FLECS_SPARSE_PAGE_BITS (4)
-// #define FLECS_ENTITY_PAGE_BITS (6)
-// #define FLECS_USE_OS_ALLOC
-// #endif
-
 /** @def FLECS_HI_COMPONENT_ID
  * This constant can be used to balance between performance and memory
  * utilization. The constant is used in two ways:
