71b0f37b6888102892451e4f7fe8f1119a3920d1
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-01-29 14:05:37 -0500

    Update FlecsTypeMapComponent.h

--- Source/FlecsLibrary/Public/flecs/Unreal/FlecsTypeMapComponent.h
+++ Source/FlecsLibrary/Public/flecs/Unreal/FlecsTypeMapComponent.h
@@ -4,13 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "FlecsScriptStructComponent.h"
-#include "FlecsScriptClassComponent.h"
-#include "FlecsScriptEnumComponent.h"
 #include "Standard/robin_hood.h"
 
 struct FFlecsTypeMapComponent final
 {
 	mutable robin_hood::unordered_flat_map<FFlecsScriptStructComponent, flecs::entity> ScriptStructMap;
-	mutable robin_hood::unordered_flat_map<FFlecsScriptClassComponent, flecs::entity> ScriptClassMap;
-	mutable robin_hood::unordered_flat_map<FFlecsScriptEnumComponent, flecs::entity> ScriptEnumMap;
 }; // struct FFlecsTypeMapComponent
