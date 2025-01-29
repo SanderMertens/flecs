f2af4a9c210f21d0a0a594ea325ca69e9768d1a2
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-01-29 01:01:41 -0500

    Component Handle Base Type

---
+++ Source/UnrealFlecs/Entities/FlecsComponentHandle.h
@@ -0,0 +1,16 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FlecsEntityHandle.h"
+#include "FlecsComponentHandle.generated.h"
+
+USTRUCT(BlueprintType, meta = (DisableSplitPin))
+struct alignas(8) UNREALFLECS_API FFlecsComponentHandle : public FFlecsEntityHandle
+{
+	GENERATED_BODY()
+
+public:
+
+}; // struct FFlecsComponentHandle
