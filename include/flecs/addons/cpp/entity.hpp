edb2bc70eebdeb413a0230dc0cd00533b680cb26
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-02-04 21:50:19 -0500

    Update entity.hpp to support different separator types in cpp

--- include/flecs/addons/cpp/entity.hpp
+++ include/flecs/addons/cpp/entity.hpp
@@ -77,6 +77,29 @@ struct entity : entity_builder<entity>
         id_ = ecs_entity_init(world, &desc);
     }
 
+    /** Create a named entity.
+     * Named entities can be looked up with the lookup functions. Entity names
+     * may be scoped, where each element in the name is separated by sep.
+     * For example: "Foo.Bar". If parts of the hierarchy in the scoped name do
+     * not yet exist, they will be automatically created.
+     *
+     * @param world The world in which to create the entity.
+     * @param name The entity name.
+     * @param sep The separator to use for the scoped name.
+     * @param root_sep The separator to use for the root of the scoped name.
+     */
+    explicit entity(world_t *world, const char *name, const char *sep, const char *root_sep)
+        : entity_builder()
+    {
+        world_ = world;
+
+        ecs_entity_desc_t desc = {};
+        desc.name = name;
+        desc.sep = sep;
+        desc.root_sep = root_sep;
+        id_ = ecs_entity_init(world, &desc);
+    }
+
     /** Conversion from flecs::entity_t to flecs::entity.
      *
      * @param id The entity_t value to convert.
