4245b15d5e70a4a4b1aaad4222c3420bc3bd8b7b
CrackedP0t <elainamartineau@gmail.com>
2025-02-06 13:41:13 -0700

    Make field::field() consistent between declaration and definition

--- include/flecs/addons/cpp/impl/field.hpp
+++ include/flecs/addons/cpp/impl/field.hpp
@@ -9,7 +9,7 @@ namespace flecs
 {
 
 template <typename T>
-inline field<T>::field(iter &iter, int32_t index) {
+inline field<T>::field(iter &iter, int index) {
     *this = iter.field<T>(index);
 }
 
