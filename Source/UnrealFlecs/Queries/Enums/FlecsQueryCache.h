0ca4cd2b21c6b714d12d51fae3e98532d89d4c3e
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-02-17 23:55:08 -0500

    Update FlecsQueryCache.h

--- Source/UnrealFlecs/Queries/Enums/FlecsQueryCache.h
+++ Source/UnrealFlecs/Queries/Enums/FlecsQueryCache.h
@@ -9,10 +9,10 @@
 UENUM(BlueprintType)
 enum class EFlecsQueryCacheType : uint8
 {
-	Default = flecs::QueryCacheDefault,
-	Auto = flecs::QueryCacheAuto,
-	All = flecs::QueryCacheAll,
-	None = flecs::QueryCacheNone
+	Default = flecs::QueryCacheDefault, /**< Behavior determined by query creation context */
+	Auto = flecs::QueryCacheAuto, /**< Cache query terms that are cacheable */
+	All = flecs::QueryCacheAll, /**< Require that all query terms can be cached */
+	None = flecs::QueryCacheNone /**< No caching */
 }; // enum class EFlecsQueryCacheType
 
 FORCEINLINE NO_DISCARD constexpr flecs::query_cache_kind_t ToFlecsQueryCache(EFlecsQueryCacheType CacheType) NOEXCEPT
