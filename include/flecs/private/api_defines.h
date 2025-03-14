297044cc434715a5191327abc2a7f33084fea04c
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-03-14 12:08:07 -0400

    Update api_defines.h

--- include/flecs/private/api_defines.h
+++ include/flecs/private/api_defines.h
@@ -262,7 +262,7 @@ typedef struct ecs_allocator_t ecs_allocator_t;
     #if defined(ECS_TARGET_CLANG) || defined(ECS_TARGET_GCC)
         #define FLECS_ALWAYS_INLINE __attribute__((always_inline))
     #elif defined(ECS_TARGET_MSVC)
-        #define FLECS_ALWAYS_INLINE __forceinline 
+        #define FLECS_ALWAYS_INLINE __forceinline
     #else
         #define FLECS_ALWAYS_INLINE
     #endif
