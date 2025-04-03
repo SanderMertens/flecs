ecff2ae102e607c88fe5158f1b73cc71cfa57af9
Sander Mertens <sander.mertens8@gmail.com>
2025-04-03 11:44:39 -0700

    Ref_t performance improvement
    Differential Revision: D72349829
    
    Pull Request resolved: https://github.com/SanderMertens/flecs/pull/1618

--- distr/flecs.c
+++ distr/flecs.c
@@ -512,6 +512,7 @@ struct ecs_table_t {
     uint64_t id;                     /* Table id in sparse set */
     ecs_flags32_t flags;             /* Flags for testing table properties */
     int16_t column_count;            /* Number of components (excluding tags) */
+    uint16_t version;                /* Version of table */
     ecs_type_t type;                 /* Vector with component ids */
 
     ecs_data_t data;                 /* Component storage */
@@ -2916,7 +2917,7 @@ void flecs_increment_table_version(
     ecs_world_t *world,
     ecs_table_t *table);
 
-uint32_t flecs_get_table_version(
+uint32_t flecs_get_table_version_fast(
     const ecs_world_t *world,
     const uint64_t table_id);
 
@@ -7987,7 +7988,8 @@ ecs_ref_t ecs_ref_init_id(
     ecs_assert(table != NULL, ECS_INTERNAL_ERROR, NULL);
 
     result.table_id = table->id;
-    result.table_version = flecs_get_table_version(world, result.table_id);
+    result.table_version_fast = flecs_get_table_version_fast(world, result.table_id);
+    result.table_version = table->version;
     result.ptr = flecs_get_component(table, ECS_RECORD_TO_ROW(record->row), 
         flecs_components_get(world, id));
 
@@ -8006,7 +8008,7 @@ void ecs_ref_update(
     ecs_check(ref->id != 0, ECS_INVALID_PARAMETER, NULL);
     ecs_check(ref->record != NULL, ECS_INVALID_PARAMETER, NULL);
 
-    if (ref->table_version == flecs_get_table_version(world, ref->table_id)) {
+    if (ref->table_version_fast == flecs_get_table_version_fast(world, ref->table_id)) {
         return;
     }
 
@@ -8014,13 +8016,20 @@ void ecs_ref_update(
     ecs_table_t *table = r->table;
     if (!table) { /* Table can be NULL, entity could have been deleted */
         ref->table_id = 0;
+        ref->table_version_fast = 0;
         ref->table_version = 0;
         ref->ptr = NULL;
         return;
     }
 
+    if (ref->table_id == table->id && ref->table_version == table->version) {
+        ref->table_version_fast = flecs_get_table_version_fast(world, ref->table_id);
+        return;
+    }
+
     ref->table_id = table->id;
-    ref->table_version = flecs_get_table_version(world, ref->table_id);
+    ref->table_version_fast = flecs_get_table_version_fast(world, ref->table_id);
+    ref->table_version = table->version;
     ref->ptr = flecs_get_component(table, ECS_RECORD_TO_ROW(r->row), 
         flecs_components_get(world, ref->id));
 
@@ -19366,10 +19375,12 @@ void flecs_increment_table_version(
     ecs_assert(table != NULL, ECS_INVALID_PARAMETER, NULL);
 
     world->table_version[table->id & ECS_TABLE_VERSION_ARRAY_BITMASK] ++;
+    table->version ++;
+
     return;
 }
 
-uint32_t flecs_get_table_version(
+uint32_t flecs_get_table_version_fast(
     const ecs_world_t *world,
     const uint64_t table_id)
 {
@@ -37511,6 +37522,7 @@ void flecs_table_init(
     }
 
     table->column_count = flecs_ito(int16_t, column_count);
+    table->version = 1;
     flecs_table_init_data(world, table);
 
     if (table->flags & EcsTableHasName) {
