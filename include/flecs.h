d65e5952feda8d5ab71148a0b1cd33ddae558367
Sander Mertens <sander.mertens8@gmail.com>
2025-06-26 15:06:35 -0700

    #1710 Implement ecs_get_ordered_children function

--- include/flecs.h
+++ include/flecs.h
@@ -3008,6 +3008,20 @@ void ecs_set_child_order(
     const ecs_entity_t *children,
     int32_t child_count);
 
+/** Get ordered children.
+ * If a parent has the OrderedChildren trait, this operation can be used to
+ * obtain the array with child entities. If this operation is used on a parent
+ * that does not have the OrderedChildren trait, it will fail.asm
+ * 
+ * @param world The world.
+ * @param parent The parent.
+ * @return The array with child entities.
+ */
+FLECS_API
+ecs_entities_t ecs_get_ordered_children(
+    const ecs_world_t *world,
+    ecs_entity_t parent);
+
 /** @} */
 
 /**
