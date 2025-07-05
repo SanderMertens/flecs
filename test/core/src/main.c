02179eac5a4bdcc607d9d74a00861b1c65e57d05
Sander Mertens <sander.mertens8@gmail.com>
2025-07-05 15:04:10 -0700

    Implement reparenting

--- test/core/src/main.c
+++ test/core/src/main.c
@@ -668,9 +668,16 @@ void Sparse_check_regular_exclusive_target_in_sparse_observer(void);
 void NonFragmentingChildOf_set_parent_no_ordered_children(void);
 void NonFragmentingChildOf_set_0_parent(void);
 void NonFragmentingChildOf_set_parent(void);
+void NonFragmentingChildOf_insert_parent(void);
 void NonFragmentingChildOf_remove_parent(void);
 void NonFragmentingChildOf_clear_child(void);
 void NonFragmentingChildOf_delete_child(void);
+void NonFragmentingChildOf_reparent(void);
+void NonFragmentingChildOf_set_parent_2_children(void);
+void NonFragmentingChildOf_remove_parent_2_children(void);
+void NonFragmentingChildOf_clear_2_children(void);
+void NonFragmentingChildOf_delete_2_children(void);
+void NonFragmentingChildOf_reparent_2_children(void);
 
 // Testsuite 'Hierarchies'
 void Hierarchies_setup(void);
@@ -5398,6 +5405,10 @@ bake_test_case NonFragmentingChildOf_testcases[] = {
         NonFragmentingChildOf_set_parent
     },
     {
+        "insert_parent",
+        NonFragmentingChildOf_insert_parent
+    },
+    {
         "remove_parent",
         NonFragmentingChildOf_remove_parent
     },
@@ -5408,6 +5419,30 @@ bake_test_case NonFragmentingChildOf_testcases[] = {
     {
         "delete_child",
         NonFragmentingChildOf_delete_child
+    },
+    {
+        "reparent",
+        NonFragmentingChildOf_reparent
+    },
+    {
+        "set_parent_2_children",
+        NonFragmentingChildOf_set_parent_2_children
+    },
+    {
+        "remove_parent_2_children",
+        NonFragmentingChildOf_remove_parent_2_children
+    },
+    {
+        "clear_2_children",
+        NonFragmentingChildOf_clear_2_children
+    },
+    {
+        "delete_2_children",
+        NonFragmentingChildOf_delete_2_children
+    },
+    {
+        "reparent_2_children",
+        NonFragmentingChildOf_reparent_2_children
     }
 };
 
@@ -13900,7 +13935,7 @@ static bake_test_suite suites[] = {
         "NonFragmentingChildOf",
         NULL,
         NULL,
-        6,
+        13,
         NonFragmentingChildOf_testcases
     },
     {
