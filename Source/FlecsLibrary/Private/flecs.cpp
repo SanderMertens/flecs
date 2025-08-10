20f90b8be01e3ff196b6e8a0a74a4228495d10f0
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-08-10 00:21:34 -0400

    Update flecs.cpp

--- Source/FlecsLibrary/Private/flecs.cpp
+++ Source/FlecsLibrary/Private/flecs.cpp
@@ -5,7 +5,5 @@ namespace flecs {
 	namespace _ {
 
 		FLECSLIBRARY_API robin_hood::unordered_map<std::string, type_impl_data> g_type_to_impl_data;
-		
-		//FLECSLIBRARY_API robin_hood::unordered_map<std::string, enum_impl_data> g_enum_to_impl_data;
 	} // namespace _
 } // namespace flecs
