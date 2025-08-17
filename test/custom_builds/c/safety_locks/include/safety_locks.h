d4c446c32bc6f19a61109caf455898d6a3870484
Indradb <60851042+Indra-db@users.noreply.github.com>
2025-08-17 22:52:21 +0900

    add CI job, add custom build test, remove temp define

---
+++ test/custom_builds/c/safety_locks/include/safety_locks.h
@@ -0,0 +1,16 @@
+#ifndef SAFETY_LOCKS_H
+#define SAFETY_LOCKS_H
+
+/* This generated file contains includes for project dependencies */
+#include "safety_locks/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
