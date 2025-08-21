5415af746ca0db76ff82f2ec938c8f267c59b3c9
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-08-21 02:35:34 -0400

    Assert for char ptr
    I forget what issue this caught but this did catch an issue at some point in my testing

--- src/misc.c
+++ src/misc.c
@@ -379,6 +379,8 @@ ecs_size_t flecs_stresc(
     const char *in) 
 {
     const char *ptr = in;
+    ecs_assert(ptr, ECS_INTERNAL_ERROR, NULL);
+    
     char ch, *bptr = out, buff[3];
     ecs_size_t written = 0;
     while ((ch = *ptr++)) {
