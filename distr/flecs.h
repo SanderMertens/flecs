d43d6cec09c287b29b2f9c087df2ed9235d18bda
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-08-31 21:37:41 -0400

    Added Owns Second Function

--- distr/flecs.h
+++ distr/flecs.h
@@ -25820,6 +25820,17 @@ struct entity_view : public id {
             _::type<First>::id(world_),
             _::type<Second>::id(world_));
     }
+    
+    /** Check if entity owns the provided pair.
+     *
+     * @param first The first element of the pair.
+     * @tparam Second The second element of the pair.
+     * @return True if the entity owns the provided component, false otherwise.
+     */
+    template <typename Second>
+    bool owns_second(flecs::entity_t first) const {
+        return owns(first, _::type<Second>::id(world_));
+    }
 
     /** Test if id is enabled.
      *
