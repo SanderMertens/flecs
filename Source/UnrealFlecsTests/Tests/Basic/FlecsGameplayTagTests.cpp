4212370d99b8996954c8abd2afd95ce7a9ebfed1
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-09-07 15:10:09 -0400

    Update FlecsGameplayTagTests.cpp

--- Source/UnrealFlecsTests/Tests/Basic/FlecsGameplayTagTests.cpp
+++ Source/UnrealFlecsTests/Tests/Basic/FlecsGameplayTagTests.cpp
@@ -44,11 +44,15 @@ TEST_CLASS_WITH_FLAGS_AND_TAGS(A6_UnrealFlecsGameplayTagTests,
 		const FFlecsEntityHandle TestEntity = FlecsWorld->CreateEntity();
 		ASSERT_THAT(IsTrue(TestEntity.IsValid()));
 
+		ASSERT_THAT(IsFalse(TestEntity.Has(TestTag)));
+
 		TestEntity.Add(TestTag);
 		ASSERT_THAT(IsTrue(TestEntity.Has(TestTag)));
+		ASSERT_THAT(IsFalse(TestEntity.Has(FFlecsTestNativeGameplayTags::Get().TestTag2)));
 
 		TestEntity.Remove(TestTag);
 		ASSERT_THAT(IsFalse(TestEntity.Has(TestTag)));
+		ASSERT_THAT(IsFalse(TestEntity.Has(FFlecsTestNativeGameplayTags::Get().TestTag2)));
 	}
 
 	TEST_METHOD(A2_GameplayTagAddRemove_TwoLevelTag)
@@ -61,9 +65,35 @@ TEST_CLASS_WITH_FLAGS_AND_TAGS(A6_UnrealFlecsGameplayTagTests,
 
 		TestEntity.Add(TestTag);
 		ASSERT_THAT(IsTrue(TestEntity.Has(TestTag)));
+		ASSERT_THAT(IsFalse(TestEntity.Has(FFlecsTestNativeGameplayTags::Get().TestTag1)));
 
 		TestEntity.Remove(TestTag);
 		ASSERT_THAT(IsFalse(TestEntity.Has(TestTag)));
+		ASSERT_THAT(IsFalse(TestEntity.Has(FFlecsTestNativeGameplayTags::Get().TestTag1)));
+	}
+
+	TEST_METHOD(A3_GameplayTagAddRemove_MultipleTags)
+	{
+		const FGameplayTag TestTag1 = FFlecsTestNativeGameplayTags::Get().TestTag1;
+		const FGameplayTag TestTag2 = FFlecsTestNativeGameplayTags::Get().TestTag2;
+		ASSERT_THAT(IsTrue(TestTag1.IsValid()));
+		ASSERT_THAT(IsTrue(TestTag2.IsValid()));
+
+		const FFlecsEntityHandle TestEntity = FlecsWorld->CreateEntity();
+		ASSERT_THAT(IsTrue(TestEntity.IsValid()));
+
+		TestEntity.Add(TestTag1);
+		TestEntity.Add(TestTag2);
+		ASSERT_THAT(IsTrue(TestEntity.Has(TestTag1)));
+		ASSERT_THAT(IsTrue(TestEntity.Has(TestTag2)));
+
+		TestEntity.Remove(TestTag1);
+		ASSERT_THAT(IsFalse(TestEntity.Has(TestTag1)));
+		ASSERT_THAT(IsTrue(TestEntity.Has(TestTag2)));
+
+		TestEntity.Remove(TestTag2);
+		ASSERT_THAT(IsFalse(TestEntity.Has(TestTag1)));
+		ASSERT_THAT(IsFalse(TestEntity.Has(TestTag2)));
 	}
 	
 }; // End of A6_UnrealFlecsGameplayTagTests
