dda67a56524409dd67082d04ef8978d340c0541e
Sander Mertens <sander.mertens8@gmail.com>
2025-09-24 17:26:35 -0700

    Add more non-fragmenting ChildOf tests

--- distr/flecs.c
+++ distr/flecs.c
@@ -78138,7 +78138,7 @@ bool flecs_query_select_w_id(
     }
 
 repeat:
-    if (!redo || !op_ctx->remaining) {
+    if (!redo || (op_ctx->remaining <= 0)) {
         tr = flecs_table_cache_next(&op_ctx->it, ecs_table_record_t);
         if (!tr) {
             return false;
@@ -82202,7 +82202,8 @@ bool flecs_query_children(
 
     if (redo) {
         if (!op_ctx->tgt) {
-            return flecs_query_and(op, redo, ctx);
+            bool result = flecs_query_and(op, redo, ctx);
+            return result;
         }
         return false;
     }
