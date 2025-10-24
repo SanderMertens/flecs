44d47703ded2fd500ecb402cc26c6746042498f0
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-10-24 00:43:17 -0400

    Added Flecs Editor Developer Class

---
+++ Source/UnrealFlecs/General/FlecsEditorDeveloperSettings.cpp
@@ -0,0 +1,9 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#include "FlecsEditorDeveloperSettings.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FlecsEditorDeveloperSettings)
+
+UFlecsEditorDeveloperSettings::UFlecsEditorDeveloperSettings()
+{
+}
---
+++ Source/UnrealFlecs/General/FlecsEditorDeveloperSettings.h
@@ -0,0 +1,21 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Engine/DeveloperSettings.h"
+
+#include "FlecsEditorDeveloperSettings.generated.h"
+
+UCLASS()
+class UNREALFLECS_API UFlecsEditorDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UFlecsEditorDeveloperSettings();
+
+	
+
+}; // class UFlecsEditorDeveloperSettings
