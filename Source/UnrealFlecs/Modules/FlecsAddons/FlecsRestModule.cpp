779eabf580928038e643a508b71a5cd48e881068
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-10-24 00:42:52 -0400

    Fix Stats Scoping

--- Source/UnrealFlecs/Modules/FlecsAddons/FlecsRestModule.cpp
+++ Source/UnrealFlecs/Modules/FlecsAddons/FlecsRestModule.cpp
@@ -12,39 +12,43 @@
 
 void UFlecsRestModule::InitializeModule(TSolidNotNull<UFlecsWorld*> InWorld, const FFlecsEntityHandle& InModuleEntity)
 {
+	// End the scope as we dont want to import a module inside another module's scope
+	InWorld->EndScope([this, InWorld]()
+	{
 #ifdef FLECS_REST
 	
-	uint16 ClientPieInstanceOffset = 0;
-	
-	const TSolidNotNull<const UWorld*> UnrealWorld = InWorld->GetWorld();
+		uint16 ClientPieInstanceOffset = 0;
+		
+		const TSolidNotNull<const UWorld*> UnrealWorld = InWorld->GetWorld();
 
-	#if WITH_EDITOR
-	
-	if (UnrealWorld->GetNetMode() == NM_Client)
-	{
-		ClientPieInstanceOffset = static_cast<uint16>(UE::GetPlayInEditorID());
-	}
-	
-	#endif // WITH_EDITOR
-	
-	const uint16 RestPort = ECS_REST_DEFAULT_PORT + ClientPieInstanceOffset;
+		#if WITH_EDITOR
+		
+		if (UnrealWorld->GetNetMode() == NM_Client)
+		{
+			ClientPieInstanceOffset = static_cast<uint16>(UE::GetPlayInEditorID());
+		}
+		
+		#endif // WITH_EDITOR
+		
+		const uint16 RestPort = ECS_REST_DEFAULT_PORT + ClientPieInstanceOffset;
 
-	InWorld->SetSingleton<flecs::Rest>(flecs::Rest{ .port = RestPort });
-	
-	RestEntity = InWorld->ObtainSingletonEntity<flecs::Rest>();
-	solid_checkf(RestEntity.IsValid(),
-		TEXT("Flecs REST module failed to create REST singleton entity on port %d."), RestPort);
+		InWorld->SetSingleton<flecs::Rest>(flecs::Rest{ .port = RestPort });
+		
+		RestEntity = InWorld->ObtainSingletonEntity<flecs::Rest>();
+		solid_checkf(RestEntity.IsValid(),
+			TEXT("Flecs REST module failed to create REST singleton entity on port %d."), RestPort);
 
-	#ifdef FLECS_STATS
+		#ifdef FLECS_STATS
 
-	if (bImportStats)
-	{
-		StatsEntity = InWorld->ImportFlecsModule<flecs::stats>();
-	}
+		if (bImportStats)
+		{
+			StatsEntity = InWorld->ImportFlecsModule<flecs::stats>();
+		}
 
-	#endif // #ifdef FLECS_STATS
+		#endif // #ifdef FLECS_STATS
 
 #endif // #ifdef FLECS_REST
+	});
 }
 
 void UFlecsRestModule::DeinitializeModule(TSolidNotNull<UFlecsWorld*> InWorld)
