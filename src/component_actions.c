a2a937bf4f1ecf91b06567f9349c4566ebdd1ca8
Sander Mertens <sander.mertens8@gmail.com>
2025-12-30 11:14:52 -0800

    Optimize cleanup of non-fragmenting trees

--- src/component_actions.c
+++ src/component_actions.c
@@ -308,14 +308,17 @@ void flecs_notify_on_add(
 
     if (added->count) {
         ecs_flags32_t table_flags = table->flags;
-        if (table_flags & EcsTableHasTraversable) {
-            flecs_emit_propagate_invalidate(world, table, row, count);
-        }
 
-        if (table_flags & EcsTableHasParent) {
-            flecs_on_non_fragmenting_child_move_add(
-                world, table, other_table, row, count);
-        }
+        // if (other_table) {
+        //     if (table_flags & EcsTableHasTraversable) {
+        //         flecs_emit_propagate_invalidate(world, table, row, count);
+        //     }
+
+        //     if (table_flags & EcsTableHasParent) {
+        //         flecs_on_non_fragmenting_child_move_add(
+        //             world, table, other_table, row, count);
+        //     }
+        // }
 
         ecs_flags32_t diff_flags = diff->added_flags;
         if (!diff_flags) {
@@ -416,6 +419,80 @@ void flecs_notify_on_remove(
             flecs_sparse_on_remove(world, table, row, count, removed);
         }
     }
+error:
+    return;
+}
+
+void flecs_simple_notify_on_remove(
+    ecs_world_t *world,
+    ecs_table_t *table,
+    ecs_table_t *other_table,
+    int32_t row,
+    int32_t count,
+    const ecs_table_diff_t *diff)
+{
+    ecs_assert(diff != NULL, ECS_INTERNAL_ERROR, NULL);
+    const ecs_type_t *removed = &diff->removed;
+    ecs_assert(count != 0, ECS_INTERNAL_ERROR, NULL);
+    ecs_assert(removed->count != 0, ECS_INTERNAL_ERROR, NULL);
+
+    // if (removed->count) {
+    //     if (!(world->flags & EcsWorldFini)) {
+    //         ecs_check(!(table->flags & EcsTableHasBuiltins), 
+    //             ECS_INVALID_OPERATION,
+    //             "removing components from builtin entities is not allowed");
+    //     }
+
+        // ecs_flags32_t table_flags = table->flags;
+        // if (table_flags & EcsTableHasTraversable) {
+        //     flecs_emit_propagate_invalidate(world, table, row, count);
+        // }
+
+        // if (other_table && (table_flags & EcsTableHasParent)) {
+        //     bool update_parent_records = true;
+        //     if (diff->added.count && (table->flags & EcsTableHasParent)) {
+        //         update_parent_records = false;
+        //     }
+
+        //     flecs_on_non_fragmenting_child_move_remove(
+        //         world, other_table, table, row, count, update_parent_records);
+        // }
+
+        ecs_flags32_t diff_flags = diff->removed_flags;
+        if (!diff_flags) {
+            return;
+        }
+
+        if (diff_flags & (EcsTableEdgeReparent|EcsTableHasOrderedChildren)) {
+            if (!other_table || !(other_table->flags & EcsTableHasChildOf)) {
+                flecs_on_unparent(world, table, other_table, row, count);
+            }
+        }
+
+        if (diff_flags & EcsTableHasDontFragment) {
+            if (flecs_dont_fragment_on_remove(
+                world, table, row, count, removed)) 
+            {
+                diff_flags |= EcsTableHasOnRemove;
+            }
+        }
+
+        if (diff_flags & EcsTableHasOnRemove) {
+            flecs_emit(world, world, &(ecs_event_desc_t) {
+                .event = EcsOnRemove,
+                .ids = removed,
+                .table = table,
+                .other_table = other_table,
+                .offset = row,
+                .count = count,
+                .observable = world
+            });
+        }
+
+        if (diff_flags & EcsTableHasSparse) {
+            flecs_sparse_on_remove(world, table, row, count, removed);
+        }
+    // }
 error:
     return;
 }
