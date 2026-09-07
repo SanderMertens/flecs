34325323235e893c63777d6a9502e5c7e2c101cf
Sander Mertens <sander.mertens8@gmail.com>
2026-09-07 15:14:04 -0700

    [cpp] Share typed component resolution for table access

--- distr/flecs.h
+++ distr/flecs.h
@@ -32118,157 +32118,19 @@ struct table {
         ecs_table_clear_entities(world_, table_);
     }
 
-    /** Find the type index for a (component) ID.
-     *
-     * @param id The (component) ID.
-     * @return The index of the ID in the table type, -1 if not found.
-     */
-    int32_t type_index(flecs::id_t id) const {
-        return ecs_table_get_type_index(world_, table_, id);
+    template <typename... T, typename... Args>
+    int32_t type_index(Args... args) const {
+        return ecs_table_get_type_index(world_, table_, _::make_id<T...>(world_, args...).id);
     }
 
-    /** Find the type index for a type.
-     *
-     * @tparam T The type.
-     * @return The index of the ID in the table type, -1 if not found.
-     */
-    template <typename T>
-    int32_t type_index() const {
-        return type_index(_::type<T>::id(world_));
+    template <typename... T, typename... Args>
+    int32_t column_index(Args... args) const {
+        return ecs_table_get_column_index(world_, table_, _::make_id<T...>(world_, args...).id);
     }
 
-    /** Find the type index for a pair.
-     *
-     * @param first First element of the pair.
-     * @param second Second element of the pair.
-     * @return The index of the ID in the table type, -1 if not found.
-     */
-    int32_t type_index(flecs::entity_t first, flecs::entity_t second) const {
-        return type_index(ecs_pair(first, second));
-    }
-
-    /** Find the type index for a pair.
-     *
-     * @tparam First First element of the pair.
-     * @param second Second element of the pair.
-     * @return The index of the ID in the table type, -1 if not found.
-     */
-    template <typename First>
-    int32_t type_index(flecs::entity_t second) const {
-        return type_index(_::type<First>::id(world_), second);
-    }
-
-    /** Find the type index for a pair.
-     *
-     * @tparam First First element of the pair.
-     * @tparam Second Second element of the pair.
-     * @return The index of the ID in the table type, -1 if not found.
-     */
-    template <typename First, typename Second>
-    int32_t type_index() const {
-        return type_index<First>(_::type<Second>::id(world_));
-    }
-
-    /** Find the column index for a (component) ID.
-     *
-     * @param id The (component) ID.
-     * @return The column index of the ID in the table, -1 if not found.
-     */
-    int32_t column_index(flecs::id_t id) const {
-        return ecs_table_get_column_index(world_, table_, id);
-    }
-
-    /** Find the column index for a type.
-     *
-     * @tparam T The type.
-     * @return The column index of the ID in the table type, -1 if not found.
-     */
-    template <typename T>
-    int32_t column_index() const {
-        return column_index(_::type<T>::id(world_));
-    }
-
-    /** Find the column index for a pair.
-     *
-     * @param first First element of the pair.
-     * @param second Second element of the pair.
-     * @return The column index of the ID in the table type, -1 if not found.
-     */
-    int32_t column_index(flecs::entity_t first, flecs::entity_t second) const {
-        return column_index(ecs_pair(first, second));
-    }
-
-    /** Find the column index for a pair.
-     *
-     * @tparam First First element of the pair.
-     * @param second Second element of the pair.
-     * @return The column index of the ID in the table type, -1 if not found.
-     */
-    template <typename First>
-    int32_t column_index(flecs::entity_t second) const {
-        return column_index(_::type<First>::id(world_), second);
-    }
-
-    /** Find the column index for a pair.
-     *
-     * @tparam First First element of the pair.
-     * @tparam Second Second element of the pair.
-     * @return The column index of the ID in the table type, -1 if not found.
-     */
-    template <typename First, typename Second>
-    int32_t column_index() const {
-        return column_index<First>(_::type<Second>::id(world_));
-    }
-
-    /** Test if the table has a (component) ID.
-     *
-     * @param id The (component) ID.
-     * @return True if the table has the ID, false if not.
-     */
-    bool has(flecs::id_t id) const {
-        return type_index(id) != -1;
-    }
-
-    /** Test if the table has the type.
-     *
-     * @tparam T The type.
-     * @return True if the table has the type, false if not.
-     */
-    template <typename T>
-    bool has() const {
-        return type_index<T>() != -1;
-    }
-
-    /** Test if the table has the pair.
-     *
-     * @param first First element of the pair.
-     * @param second Second element of the pair.
-     * @return True if the table has the pair, false if not.
-     */
-    bool has(flecs::entity_t first, flecs::entity_t second) const {
-        return type_index(first, second) != -1;
-    }
-
-    /** Test if the table has the pair.
-     *
-     * @tparam First First element of the pair.
-     * @param second Second element of the pair.
-     * @return True if the table has the pair, false if not.
-     */
-    template <typename First>
-    bool has(flecs::entity_t second) const {
-        return type_index<First>(second) != -1;
-    }
-
-    /** Test if the table has the pair.
-     *
-     * @tparam First First element of the pair.
-     * @tparam Second Second element of the pair.
-     * @return True if the table has the pair, false if not.
-     */
-    template <typename First, typename Second>
-    bool has() const {
-        return type_index<First, Second>() != -1;
+    template <typename... T, typename... Args>
+    bool has(Args... args) const {
+        return type_index<T...>(args...) != -1;
     }
 
     /** Get a pointer to the component array by column index.
@@ -32280,133 +32142,14 @@ struct table {
         return ecs_table_get_column(table_, index, 0);
     }
 
-    /* get */
-
-    /** Get a pointer to the component array by component.
-     *
-     * @param id The component ID.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    void* try_get(flecs::id_t id) const {
-        int32_t index = column_index(id);
-        if (index == -1) {
-            return nullptr;
-        }
-        return get_column(index);
+    template <typename... T, typename... Args>
+    auto try_get(Args... args) const {
+        return get_ptr<false>(_::make_id<T...>(world_, args...));
     }
 
-    /** Get a pointer to the component array by pair.
-     *
-     * @param first The first element of the pair.
-     * @param second The second element of the pair.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    void* try_get(flecs::entity_t first, flecs::entity_t second) const {
-        return try_get(ecs_pair(first, second));
-    }
-
-    /** Get a pointer to the component array by component.
-     *
-     * @tparam T The component.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename T, if_t< is_actual<T>::value > = 0>
-    T* try_get() const {
-        return static_cast<T*>(try_get(_::type<T>::id(world_)));
-    }
-
-    /** Get a pointer to the component array by component.
-     *
-     * @tparam T The component.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename T, typename A = actual_type_t<T>,
-        if_t< flecs::is_pair<T>::value > = 0>
-    A* try_get() const {
-        return static_cast<A*>(try_get(_::type<T>::id(world_)));
-    }
-
-    /** Get a pointer to the component array by pair.
-     *
-     * @tparam First The first element of the pair.
-     * @param second The second element of the pair.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename First>
-    First* try_get(flecs::entity_t second) const {
-        return static_cast<First*>(try_get(_::type<First>::id(world_), second));
-    }
-
-    /* get */
-
-    /** Get a pointer to the component array by component.
-     *
-     * @param id The component ID.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    void* get(flecs::id_t id) const {
-        int32_t index = column_index(id);
-        if (index == -1) {
-            return nullptr;
-        }
-        void *r = get_column(index);
-        ecs_assert(r != nullptr, ECS_INVALID_OPERATION,
-            "invalid get: table does not have component (use try_get())");
-        return r;
-    }
-
-    /** Get a pointer to the component array by pair.
-     *
-     * @param first The first element of the pair.
-     * @param second The second element of the pair.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    void* get(flecs::entity_t first, flecs::entity_t second) const {
-        return get(ecs_pair(first, second));
-    }
-
-    /** Get a pointer to the component array by component.
-     *
-     * @tparam T The component.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename T, if_t< is_actual<T>::value > = 0>
-    T* get() const {
-        return static_cast<T*>(get(_::type<T>::id(world_)));
-    }
-
-    /** Get a pointer to the component array by component.
-     *
-     * @tparam T The component.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename T, typename A = actual_type_t<T>,
-        if_t< flecs::is_pair<T>::value > = 0>
-    A* get() const {
-        return static_cast<A*>(get(_::type<T>::id(world_)));
-    }
-
-    /** Get a pointer to the component array by pair.
-     *
-     * @tparam First The first element of the pair.
-     * @param second The second element of the pair.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename First>
-    First* get(flecs::entity_t second) const {
-        return static_cast<First*>(get(_::type<First>::id(world_), second));
-    }
-
-    /** Get a pointer to the component array by pair.
-     *
-     * @tparam First The first element of the pair.
-     * @tparam Second The second element of the pair.
-     * @return Pointer to the column, nullptr if not found.
-     */
-    template <typename First, typename Second, typename P = flecs::pair<First, Second>,
-        typename A = actual_type_t<P>, if_not_t< flecs::is_pair<First>::value> = 0>
-    A* get() const {
-        return static_cast<A*>(get<First>(_::type<Second>::id(world_)));
+    template <typename... T, typename... Args>
+    auto get(Args... args) const {
+        return get_ptr<true>(_::make_id<T...>(world_, args...));
     }
 
     /** Get the column size.
@@ -32483,6 +32226,21 @@ struct table {
     /** Implicit conversion to table_t*. */
     operator table_t*() const {
         return table_;
+    }
+
+private:
+    template <bool Required, typename Id>
+    typename Id::type* get_ptr(Id id) const {
+        int32_t index = column_index(id.id);
+        if (index == -1) {
+            return nullptr;
+        }
+        auto ptr = static_cast<typename Id::type*>(get_column(index));
+        if constexpr (Required) {
+            ecs_assert(ptr != nullptr, ECS_INVALID_OPERATION,
+                "invalid get: table does not have component (use try_get())");
+        }
+        return ptr;
     }
 
 protected:
