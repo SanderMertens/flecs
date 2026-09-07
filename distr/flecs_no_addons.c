9e23abdde6937c045d3ba3a6b32f95554fe9de22
Sander Mertens <sander.mertens8@gmail.com>
2026-09-07 14:08:59 -0700

    [core] Update owned type arrays in place through shared insertion and removal

--- distr/flecs_no_addons.c
+++ distr/flecs_no_addons.c
@@ -32437,210 +32437,85 @@ static int flecs_type_find(
     return -1;
 }
 
-static int flecs_type_find_ignoring_generation(
-    const ecs_type_t *type,
-    ecs_id_t id)
-{
-    ecs_id_t *array = type->array;
-    int32_t i, count = type->count;
-
-    for (i = 0; i < count; i ++) {
-        if ((uint32_t)array[i] == (uint32_t)id) {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-/* Count number of matching ids */
-static int flecs_type_count_matches(
-    const ecs_type_t *type,
-    ecs_id_t wildcard,
-    int32_t offset)
-{
-    ecs_id_t *array = type->array;
-    int32_t i = offset, count = type->count;
-
-    for (; i < count; i ++) {
-        ecs_id_t cur = array[i];
-        if (!ecs_id_match(cur, wildcard)) {
-            break;
-        }
-    }
-
-    return i - offset;
-}
-
-/* Create type from source type with id */
 static int flecs_type_new_with(
     ecs_world_t *world,
     ecs_type_t *dst,
     const ecs_type_t *src,
     ecs_id_t with)
 {
-    ecs_id_t *src_array = src->array;
     int32_t at = flecs_type_find_insert(src, 0, with);
     if (at == -1) {
         return -1;
     }
 
-    int32_t dst_count = src->count + 1;
-    ecs_id_t *dst_array = flecs_walloc_n(world, ecs_id_t, dst_count);
-    dst->count = dst_count;
-    dst->array = dst_array;
-
-    if (at) {
-        ecs_os_memcpy_n(dst_array, src_array, ecs_id_t, at);
+    int32_t count = src->count;
+    ecs_id_t *array;
+    if (dst == src) {
+        array = flecs_wrealloc_n(world, ecs_id_t, count + 1, count, src->array);
+        ecs_os_memmove_n(array + at + 1, array + at, ecs_id_t, count - at);
+    } else {
+        array = flecs_walloc_n(world, ecs_id_t, count + 1);
+        if (at) {
+            ecs_os_memcpy_n(array, src->array, ecs_id_t, at);
+        }
+        if (count != at) {
+            ecs_os_memcpy_n(array + at + 1, src->array + at, ecs_id_t, count - at);
+        }
     }
-
-    int32_t remain = src->count - at;
-    if (remain) {
-        ecs_os_memcpy_n(&dst_array[at + 1], &src_array[at], ecs_id_t, remain);
-    }
-
-    dst_array[at] = with;
-
+    array[at] = with;
+    *dst = (ecs_type_t){ .array = array, .count = count + 1 };
     return 0;
 }
 
-/* Create type from source type without ids matching wildcard */
-static int flecs_type_new_filtered(
-    ecs_world_t *world,
-    ecs_type_t *dst,
-    const ecs_type_t *src,
-    ecs_id_t wildcard,
-    int32_t at)
-{
-    *dst = flecs_type_copy(world, src);
-    ecs_id_t *dst_array = dst->array;
-    ecs_id_t *src_array = src->array;
-    if (at) {
-        ecs_assert(dst_array != NULL, ECS_INTERNAL_ERROR, NULL);
-        ecs_os_memcpy_n(dst_array, src_array, ecs_id_t, at);
-    }
-
-    int32_t i = at + 1, w = at, count = src->count;
-    for (; i < count; i ++) {
-        ecs_id_t id = src_array[i];
-        if (!ecs_id_match(id, wildcard)) {
-            dst_array[w] = id;
-            w ++;
-        }
-    }
-
-    dst->count = w;
-
-    if (w != count) {
-        if (w) {
-            dst->array = flecs_wrealloc_n(world, ecs_id_t, w, count, dst->array);
-        } else {
-            flecs_wfree_n(world, ecs_id_t, count, dst->array);
-            dst->array = NULL;
-        }
-    }
-
-    return 0;
-}
-
-/* Create type from source type without id */
 static int flecs_type_new_without(
     ecs_world_t *world,
     ecs_type_t *dst,
     const ecs_type_t *src,
     ecs_id_t without)
 {
-    ecs_id_t *src_array = src->array;
-    int32_t count = 1, at = flecs_type_find(src, without);
+    int32_t at = flecs_type_find(src, without);
     if (at == -1) {
         return -1;
     }
-
-    int32_t src_count = src->count;
-    if (src_count == 1) {
-        dst->array = NULL;
-        dst->count = 0;
-        return 0;
-    }
-
-    if (ecs_id_is_wildcard(without)) {
-        if (ECS_IS_PAIR(without)) {
-            ecs_entity_t r = ECS_PAIR_FIRST(without);
-            ecs_entity_t o = ECS_PAIR_SECOND(without);
-            if (r == EcsWildcard && o != EcsWildcard) {
-                return flecs_type_new_filtered(world, dst, src, without, at);
-            }
+    int32_t count = src->count, removed = 1;
+    bool wildcard = ecs_id_is_wildcard(without);
+    if (wildcard) {
+        for (int32_t i = at + 1; i < count; i ++) {
+            removed += ecs_id_match(src->array[i], without);
         }
-        count += flecs_type_count_matches(src, without, at + 1);
     }
 
-    int32_t dst_count = src_count - count;
-    dst->count = dst_count;
-    if (!dst_count) {
-        dst->array = NULL;
-        return 0;
+    int32_t dst_count = count - removed;
+    ecs_id_t *array = src->array;
+    if (dst != src) {
+        array = dst_count ? flecs_walloc_n(world, ecs_id_t, dst_count) : NULL;
+        if (at) {
+            ecs_os_memcpy_n(array, src->array, ecs_id_t, at);
+        }
     }
-
-    ecs_id_t *dst_array = flecs_walloc_n(world, ecs_id_t, dst_count);
-    dst->array = dst_array;
-
-    ecs_assert(dst_array != NULL, ECS_INTERNAL_ERROR, NULL);
-    ecs_assert(src_array != NULL, ECS_INTERNAL_ERROR, NULL);
-
-    if (at) {
-        ecs_os_memcpy_n(dst_array, src_array, ecs_id_t, at);
+    if (dst_count > at) {
+        if (wildcard) {
+            int32_t w = at;
+            for (int32_t i = at + 1; i < count; i ++) {
+                ecs_id_t id = src->array[i];
+                if (!ecs_id_match(id, without)) {
+                    array[w ++] = id;
+                }
+            }
+        } else {
+            ecs_os_memmove_n(array + at, src->array + at + 1,
+                ecs_id_t, dst_count - at);
+        }
     }
-
-    int32_t remain = dst_count - at;
-    if (remain) {
-        ecs_os_memcpy_n(
-            &dst_array[at], &src_array[at + count], ecs_id_t, remain);
+    if (dst == src) {
+        if (dst_count) {
+            array = flecs_wrealloc_n(world, ecs_id_t, dst_count, count, array);
+        } else {
+            flecs_wfree_n(world, ecs_id_t, count, array);
+            array = NULL;
+        }
     }
-
-    return 0;
-}
-
-/* Create type from source type without entity id, ignoring generation */
-static int flecs_type_new_without_ignoring_generation(
-    ecs_world_t *world,
-    ecs_type_t *dst,
-    const ecs_type_t *src,
-    ecs_id_t without)
-{
-    ecs_assert(!ecs_id_is_wildcard(without), ECS_INVALID_PARAMETER, NULL);
-    ecs_assert(!ECS_IS_PAIR(without), ECS_INVALID_PARAMETER, NULL);
-
-    ecs_id_t *src_array = src->array;
-    int32_t at = flecs_type_find_ignoring_generation(src, without);
-    if (at == -1) {
-        return -1;
-    }
-
-    int32_t src_count = src->count;
-    if (src_count == 1) {
-        dst->array = NULL;
-        dst->count = 0;
-        return 0;
-    }
-
-    int32_t dst_count = src_count - 1;
-    ecs_id_t *dst_array = flecs_walloc_n(world, ecs_id_t, dst_count);
-    dst->array = dst_array;
-    dst->count = dst_count;
-
-    ecs_assert(dst_array != NULL, ECS_INTERNAL_ERROR, NULL);
-    ecs_assert(src_array != NULL, ECS_INTERNAL_ERROR, NULL);
-
-    if (at) {
-        ecs_os_memcpy_n(dst_array, src_array, ecs_id_t, at);
-    }
-
-    int32_t remain = dst_count - at;
-    if (remain) {
-        ecs_os_memcpy_n(
-            &dst_array[at], &src_array[at + 1], ecs_id_t, remain);
-    }
-
+    *dst = (ecs_type_t){ .array = array, .count = dst_count };
     return 0;
 }
 
@@ -32673,49 +32548,34 @@ void flecs_type_free(
     }
 }
 
-/* Add to type */
 void flecs_type_add(
     ecs_world_t *world,
     ecs_type_t *type,
     ecs_id_t add)
 {
-    ecs_type_t new_type;
-    int res = flecs_type_new_with(world, &new_type, type, add);
-    if (res != -1) {
-        flecs_type_free(world, type);
-        type->array = new_type.array;
-        type->count = new_type.count;
-    }
+    flecs_type_new_with(world, type, type, add);
 }
 
-/* Remove from type */
 static void flecs_type_remove(
     ecs_world_t *world,
     ecs_type_t *type,
     ecs_id_t remove)
 {
-    ecs_type_t new_type;
-    int res = flecs_type_new_without(world, &new_type, type, remove);
-    if (res != -1) {
-        flecs_type_free(world, type);
-        type->array = new_type.array;
-        type->count = new_type.count;
-    }
+    flecs_type_new_without(world, type, type, remove);
 }
 
-/* Remove from type while ignoring entity id generation */
 void flecs_type_remove_ignoring_generation(
     ecs_world_t *world,
     ecs_type_t *type,
     ecs_id_t remove)
 {
-    ecs_type_t new_type;
-    int res = flecs_type_new_without_ignoring_generation(
-        world, &new_type, type, remove);
-    if (res != -1) {
-        flecs_type_free(world, type);
-        type->array = new_type.array;
-        type->count = new_type.count;
+    ecs_assert(!ecs_id_is_wildcard(remove), ECS_INVALID_PARAMETER, NULL);
+    ecs_assert(!ECS_IS_PAIR(remove), ECS_INVALID_PARAMETER, NULL);
+    for (int32_t i = 0; i < type->count; i ++) {
+        if ((uint32_t)type->array[i] == (uint32_t)remove) {
+            flecs_type_remove(world, type, type->array[i]);
+            return;
+        }
     }
 }
 
