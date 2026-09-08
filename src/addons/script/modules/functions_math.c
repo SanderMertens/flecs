60b399e134b1f86946b1bc994f15a603fe608a91
Sander Mertens <sander.mertens8@gmail.com>
2026-09-07 18:20:38 -0700

    [script] Register native math functions from a shared signature catalog

--- src/addons/script/modules/functions_math.c
+++ src/addons/script/modules/functions_math.c
@@ -368,128 +368,78 @@ FLECS_SCRIPT_LENGTH_SQ(double)
 FLECS_SCRIPT_NORMALIZE(float)
 FLECS_SCRIPT_NORMALIZE(double)
 
-#define FLECS_MATH_FUNC_F64(name, ...)\
-    static\
-    void flecs_math_##name(\
-        const ecs_function_ctx_t *ctx,\
-        int32_t argc,\
-        const ecs_value_t *argv,\
-        ecs_value_t *result)\
+typedef struct flecs_math_function_t {
+    const char *name;
+    const char *brief;
+    double (*unary)(double);
+    double (*binary)(double, double);
+    double (*exponent)(double, int);
+    ecs_function_callback_t callback;
+} flecs_math_function_t;
+
+#define FLECS_MATH_INTRINSIC(name, expr)\
+    static void flecs_math_##name(\
+        const ecs_function_ctx_t *ctx, int32_t argc,\
+        const ecs_value_t *argv, ecs_value_t *result)\
     {\
         (void)ctx; (void)argc;\
-        ecs_assert(argc == 1, ECS_INTERNAL_ERROR, NULL);\
         double x = *(double*)argv[0].ptr;\
-        *(double*)result->ptr = __VA_ARGS__;\
+        *(double*)result->ptr = expr;\
     }
 
-#define FLECS_MATH_FUNC_F64_F64(name, ...)\
-    static\
-    void flecs_math_##name(\
-        const ecs_function_ctx_t *ctx,\
-        int32_t argc,\
-        const ecs_value_t *argv,\
-        ecs_value_t *result)\
-    {\
-        (void)ctx; (void)argc;\
-        ecs_assert(argc == 2, ECS_INTERNAL_ERROR, NULL);\
-        double x = *(double*)argv[0].ptr;\
-        double y = *(double*)argv[1].ptr;\
-        *(double*)result->ptr = __VA_ARGS__;\
+FLECS_MATH_INTRINSIC(sqrt, sqrt(x))
+FLECS_MATH_INTRINSIC(sqr, x * x)
+FLECS_MATH_INTRINSIC(ceil, ceil(x))
+FLECS_MATH_INTRINSIC(floor, floor(x))
+FLECS_MATH_INTRINSIC(round, round(x))
+FLECS_MATH_INTRINSIC(abs, fabs(x))
+
+static const flecs_math_function_t flecs_math_functions[] = {
+    {"cos", "Compute cosine", .unary = cos},
+    {"sin", "Compute sine", .unary = sin},
+    {"tan", "Compute tangent", .unary = tan},
+    {"acos", "Compute arc cosine", .unary = acos},
+    {"asin", "Compute arc sine", .unary = asin},
+    {"atan", "Compute arc tangent", .unary = atan},
+    {"atan2", "Compute arc tangent with two parameters", .binary = atan2},
+    {"cosh", "Compute hyperbolic cosine", .unary = cosh},
+    {"sinh", "Compute hyperbolic sine", .unary = sinh},
+    {"tanh", "Compute hyperbolic tangent", .unary = tanh},
+    {"acosh", "Compute area hyperbolic cosine", .unary = acosh},
+    {"asinh", "Compute area hyperbolic sine", .unary = asinh},
+    {"atanh", "Compute area hyperbolic tangent", .unary = atanh},
+    {"exp", "Compute exponential function", .unary = exp},
+    {"ldexp", "Generate value from significand and exponent", .exponent = ldexp},
+    {"log", "Compute natural logarithm", .unary = log},
+    {"log10", "Compute common logarithm", .unary = log10},
+    {"exp2", "Compute binary exponential function", .unary = exp2},
+    {"log2", "Compute binary logarithm", .unary = log2},
+    {"pow", "Raise to power", .binary = pow},
+    {"sqrt", "Compute square root", .callback = flecs_math_sqrt},
+    {"sqr", "Compute square", .callback = flecs_math_sqr},
+    {"ceil", "Round up value", .callback = flecs_math_ceil},
+    {"floor", "Round down value", .callback = flecs_math_floor},
+    {"round", "Round to nearest", .callback = flecs_math_round},
+    {"abs", "Compute absolute value", .callback = flecs_math_abs},
+};
+
+static void flecs_math_call(
+    const ecs_function_ctx_t *ctx,
+    int32_t argc,
+    const ecs_value_t *argv,
+    ecs_value_t *result)
+{
+    (void)argc;
+    const flecs_math_function_t *fn = ctx->ctx;
+    double x = *(double*)argv[0].ptr;
+    if (fn->unary) {
+        *(double*)result->ptr = fn->unary(x);
+    } else if (fn->binary) {
+        *(double*)result->ptr = fn->binary(x, *(double*)argv[1].ptr);
+    } else {
+        *(double*)result->ptr = fn->exponent(x, *(int32_t*)argv[1].ptr);
     }
-
-#define FLECS_MATH_FUNC_F64_I32(name, ...)\
-    static\
-    void flecs_math_##name(\
-        const ecs_function_ctx_t *ctx,\
-        int32_t argc,\
-        const ecs_value_t *argv,\
-        ecs_value_t *result)\
-    {\
-        (void)ctx; (void)argc;\
-        ecs_assert(argc == 2, ECS_INTERNAL_ERROR, NULL);\
-        double x = *(double*)argv[0].ptr;\
-        ecs_i32_t y = *(ecs_i32_t*)argv[1].ptr;\
-        *(double*)result->ptr = __VA_ARGS__;\
-    }
-
-#define FLECS_MATH_FUNC_DEF_F64(_name, brief)\
-    {\
-        ecs_entity_t f = ecs_function(world, {\
-            .name = #_name,\
-            .parent = ecs_id(FlecsScriptMath),\
-            .return_type = ecs_id(ecs_f64_t),\
-            .params = {{ .name = "x", .type = ecs_id(ecs_f64_t) }},\
-            .callback = flecs_math_##_name\
-        });\
-        ecs_doc_set_brief(world, f, brief);\
-    }
-
-#define FLECS_MATH_FUNC_DEF_F64_F64(_name, brief)\
-    {\
-        ecs_entity_t f = ecs_function(world, {\
-            .name = #_name,\
-            .parent = ecs_id(FlecsScriptMath),\
-            .return_type = ecs_id(ecs_f64_t),\
-            .params = {\
-                { .name = "x", .type = ecs_id(ecs_f64_t) },\
-                { .name = "y", .type = ecs_id(ecs_f64_t) }\
-            },\
-            .callback = flecs_math_##_name\
-        });\
-        ecs_doc_set_brief(world, f, brief);\
-    }
-
-#define FLECS_MATH_FUNC_DEF_F64_F32(_name, brief)\
-    {\
-        ecs_entity_t f = ecs_function(world, {\
-            .name = #_name,\
-            .parent = ecs_id(FlecsScriptMath),\
-            .return_type = ecs_id(ecs_f64_t),\
-            .params = {\
-                { .name = "x", .type = ecs_id(ecs_f64_t) },\
-                { .name = "y", .type = ecs_id(ecs_i32_t) }\
-            },\
-            .callback = flecs_math_##_name\
-        });\
-        ecs_doc_set_brief(world, f, brief);\
-    }
-
-/* Trigonometric functions */
-FLECS_MATH_FUNC_F64(cos, cos(x))
-FLECS_MATH_FUNC_F64(sin, sin(x))
-FLECS_MATH_FUNC_F64(tan, tan(x))
-FLECS_MATH_FUNC_F64(acos, acos(x))
-FLECS_MATH_FUNC_F64(asin, asin(x))
-FLECS_MATH_FUNC_F64(atan, atan(x))
-FLECS_MATH_FUNC_F64_F64(atan2, atan2(x, y))
-
-/* Hyperbolic functions */
-FLECS_MATH_FUNC_F64(cosh, cosh(x))
-FLECS_MATH_FUNC_F64(sinh, sinh(x))
-FLECS_MATH_FUNC_F64(tanh, tanh(x))
-FLECS_MATH_FUNC_F64(acosh, acosh(x))
-FLECS_MATH_FUNC_F64(asinh, asinh(x))
-FLECS_MATH_FUNC_F64(atanh, atanh(x))
-
-/* Exponential and logarithmic functions */
-FLECS_MATH_FUNC_F64(exp, exp(x))
-FLECS_MATH_FUNC_F64_I32(ldexp, ldexp(x, y))
-FLECS_MATH_FUNC_F64(log, log(x))
-FLECS_MATH_FUNC_F64(log10, log10(x))
-FLECS_MATH_FUNC_F64(exp2, exp2(x))
-FLECS_MATH_FUNC_F64(log2, log2(x))
-
-/* Power functions */
-FLECS_MATH_FUNC_F64_F64(pow, pow(x, y))
-FLECS_MATH_FUNC_F64(sqrt, sqrt(x))
-FLECS_MATH_FUNC_F64(sqr, x * x)
-
-/* Rounding functions */
-FLECS_MATH_FUNC_F64(ceil, ceil(x))
-FLECS_MATH_FUNC_F64(floor, floor(x))
-FLECS_MATH_FUNC_F64(round, round(x))
-
-FLECS_MATH_FUNC_F64(abs, fabs(x))
+}
 
 void FlecsScriptMathImport(
     ecs_world_t *world)
@@ -515,42 +465,26 @@ void FlecsScriptMathImport(
         .value = &PI
     });
 
-    /* Trigonometric functions */
-    FLECS_MATH_FUNC_DEF_F64(cos, "Compute cosine");
-    FLECS_MATH_FUNC_DEF_F64(sin, "Compute sine");
-    FLECS_MATH_FUNC_DEF_F64(tan, "Compute tangent");
-    FLECS_MATH_FUNC_DEF_F64(acos, "Compute arc cosine");
-    FLECS_MATH_FUNC_DEF_F64(asin, "Compute arc sine");
-    FLECS_MATH_FUNC_DEF_F64(atan, "Compute arc tangent");
-    FLECS_MATH_FUNC_DEF_F64_F64(atan2, "Compute arc tangent with two parameters");
-
-    /* Hyperbolic functions */
-    FLECS_MATH_FUNC_DEF_F64(cosh, "Compute hyperbolic cosine");
-    FLECS_MATH_FUNC_DEF_F64(sinh, "Compute hyperbolic sine");
-    FLECS_MATH_FUNC_DEF_F64(tanh, "Compute hyperbolic tangent");
-    FLECS_MATH_FUNC_DEF_F64(acosh, "Compute area hyperbolic cosine");
-    FLECS_MATH_FUNC_DEF_F64(asinh, "Compute area hyperbolic sine");
-    FLECS_MATH_FUNC_DEF_F64(atanh, "Compute area hyperbolic tangent");
-
-    /* Exponential and logarithmic functions */
-    FLECS_MATH_FUNC_DEF_F64(exp, "Compute exponential function");
-    FLECS_MATH_FUNC_DEF_F64_F32(ldexp, "Generate value from significand and exponent");
-    FLECS_MATH_FUNC_DEF_F64(log, "Compute natural logarithm");
-    FLECS_MATH_FUNC_DEF_F64(log10, "Compute common logarithm");
-    FLECS_MATH_FUNC_DEF_F64(exp2, "Compute binary exponential function");
-    FLECS_MATH_FUNC_DEF_F64(log2, "Compute binary logarithm");
-
-    /* Power functions */
-    FLECS_MATH_FUNC_DEF_F64_F64(pow, "Raise to power");
-    FLECS_MATH_FUNC_DEF_F64(sqrt, "Compute square root");
-    FLECS_MATH_FUNC_DEF_F64(sqr, "Compute square");
-
-    /* Rounding functions */
-    FLECS_MATH_FUNC_DEF_F64(ceil, "Round up value");
-    FLECS_MATH_FUNC_DEF_F64(floor, "Round down value");
-    FLECS_MATH_FUNC_DEF_F64(round, "Round to nearest");
-
-    FLECS_MATH_FUNC_DEF_F64(abs, "Compute absolute value");
+    for (int32_t i = 0; i < (int32_t)(sizeof(flecs_math_functions) /
+        sizeof(flecs_math_functions[0])); i ++)
+    {
+        const flecs_math_function_t *fn = &flecs_math_functions[i];
+        ecs_function_desc_t desc = {
+            .name = fn->name,
+            .parent = ecs_id(FlecsScriptMath),
+            .return_type = ecs_id(ecs_f64_t),
+            .params = {{ .name = "x", .type = ecs_id(ecs_f64_t) }},
+            .callback = fn->callback ? fn->callback : flecs_math_call,
+            .ctx = ECS_CONST_CAST(flecs_math_function_t*, fn)
+        };
+        if (fn->binary || fn->exponent) {
+            desc.params[1].name = "y";
+            desc.params[1].type = fn->exponent
+                ? ecs_id(ecs_i32_t) : ecs_id(ecs_f64_t);
+        }
+        ecs_entity_t f = ecs_function_init(world, &desc);
+        ecs_doc_set_brief(world, f, fn->brief);
+    }
 
     ecs_set_name_prefix(world, "EcsScript");
 
